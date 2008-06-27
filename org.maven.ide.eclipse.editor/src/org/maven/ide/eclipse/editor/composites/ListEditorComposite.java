d4d262feb55c9c92c17cfab4426d25ee0b797b00
dip <dip@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-06-27 06:18:30 +0000

    cell editor width fix

--- org.maven.ide.eclipse.editor/src/org/maven/ide/eclipse/editor/composites/ListEditorComposite.java
+++ org.maven.ide.eclipse.editor/src/org/maven/ide/eclipse/editor/composites/ListEditorComposite.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ICellModifier;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.IOpenListener;
@@ -21,17 +20,20 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.forms.widgets.FormToolkit;
+
 
 /**
  * List editor composite
@@ -41,8 +43,9 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 public class ListEditorComposite<T> extends Composite {
 
   TableViewer viewer;
-  
+
   Button addButton;
+
   Button removeButton;
 
   boolean readOnly = false;
@@ -51,50 +54,52 @@ public class ListEditorComposite<T> extends Composite {
     super(parent, style);
 
     FormToolkit toolkit = new FormToolkit(parent.getDisplay());
-    
+
     GridLayout gridLayout = new GridLayout(2, false);
     gridLayout.marginWidth = 1;
     gridLayout.marginHeight = 1;
     setLayout(gridLayout);
 
-    Table table = toolkit.createTable(this, SWT.FLAT | SWT.MULTI | style);
-    
-    TableLayout tableLayout = new TableLayout();
-    tableLayout.addColumnData(new ColumnWeightData(100, 200, true));
-    table.setLayout(tableLayout);
-    
+    final Table table = toolkit.createTable(this, SWT.FLAT | SWT.MULTI | style);
+    final TableColumn column = new TableColumn(table, SWT.NONE);
+    table.addControlListener(new ControlAdapter() {
+      public void controlResized(ControlEvent e) {
+        column.setWidth(table.getClientArea().width);
+      }
+    });
+
     viewer = new TableViewer(table);
-    
+
     GridData viewerData = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2);
     viewerData.widthHint = 100;
     viewerData.heightHint = 50;
     viewerData.minimumHeight = 50;
     table.setLayoutData(viewerData);
-    
+
     viewer.setData(FormToolkit.KEY_DRAW_BORDER, Boolean.TRUE);
     // table.setData(FormToolkit.KEY_DRAW_BORDER, Boolean.TRUE);
-    
+
     addButton = toolkit.createButton(this, "Add...", SWT.FLAT);
     addButton.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, false));
     addButton.setEnabled(false);
-    
+
     removeButton = toolkit.createButton(this, "Delete", SWT.FLAT);
     removeButton.setLayoutData(new GridData(SWT.FILL, SWT.TOP, false, true));
     removeButton.setEnabled(false);
-    
+
     viewer.addSelectionChangedListener(new ISelectionChangedListener() {
       public void selectionChanged(SelectionChangedEvent event) {
         updateRemoveButton();
       }
     });
-    
+
     toolkit.paintBordersFor(this);
   }
-  
+
   public void setLabelProvider(ILabelProvider labelProvider) {
     viewer.setLabelProvider(labelProvider);
   }
-  
+
   public void setContentProvider(ListEditorContentProvider<T> contentProvider) {
     viewer.setContentProvider(contentProvider);
   }
@@ -103,20 +108,20 @@ public class ListEditorComposite<T> extends Composite {
     viewer.setInput(input);
     viewer.setSelection(new StructuredSelection());
   }
-  
+
   public void setOpenListener(IOpenListener listener) {
     viewer.addOpenListener(listener);
   }
-  
+
   public void addSelectionListener(ISelectionChangedListener listener) {
     viewer.addSelectionChangedListener(listener);
   }
-  
+
   public void setAddListener(SelectionListener listener) {
     addButton.addSelectionListener(listener);
     addButton.setEnabled(true);
   }
-  
+
   public void setRemoveListener(SelectionListener listener) {
     removeButton.addSelectionListener(listener);
   }
@@ -128,7 +133,7 @@ public class ListEditorComposite<T> extends Composite {
   @SuppressWarnings("unchecked")
   public List<T> getSelection() {
     IStructuredSelection selection = (IStructuredSelection) viewer.getSelection();
-    return selection==null ? Collections.emptyList() : selection.toList();
+    return selection == null ? Collections.emptyList() : selection.toList();
   }
 
   public void setSelection(List<T> selection) {
@@ -140,7 +145,7 @@ public class ListEditorComposite<T> extends Composite {
     addButton.setEnabled(!readOnly);
     updateRemoveButton();
   }
-  
+
   public void refresh() {
     if(!viewer.getTable().isDisposed()) {
       viewer.refresh(true);
@@ -149,15 +154,13 @@ public class ListEditorComposite<T> extends Composite {
 
   public void setCellModifier(ICellModifier cellModifier) {
     viewer.setColumnProperties(new String[] {"?"});
-    
+
     TextCellEditor editor = new TextCellEditor(viewer.getTable());
     viewer.setCellEditors(new CellEditor[] {editor});
     viewer.setCellModifier(cellModifier);
   }
 
-
   private void updateRemoveButton() {
     removeButton.setEnabled(!readOnly && !viewer.getSelection().isEmpty());
   }
 }
-
