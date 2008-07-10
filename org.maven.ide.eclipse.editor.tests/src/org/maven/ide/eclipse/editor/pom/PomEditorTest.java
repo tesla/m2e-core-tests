80aea558cbbd515725fd978446bd198f2aea8db1
akraev <akraev@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-07-10 03:03:15 +0000

    refactored; added test case that passes

--- org.maven.ide.eclipse.editor.tests/src/org/maven/ide/eclipse/editor/pom/PomEditorTest.java
+++ org.maven.ide.eclipse.editor.tests/src/org/maven/ide/eclipse/editor/pom/PomEditorTest.java
@@ -9,14 +9,15 @@
 package org.maven.ide.eclipse.editor.pom;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Tree;
 
 import com.windowtester.runtime.IUIContext;
 import com.windowtester.runtime.WT;
+import com.windowtester.runtime.WaitTimedOutException;
+import com.windowtester.runtime.WidgetSearchException;
 import com.windowtester.runtime.condition.HasTextCondition;
-import com.windowtester.runtime.locator.WidgetReference;
 import com.windowtester.runtime.swt.UITestCaseSWT;
 import com.windowtester.runtime.swt.condition.shell.ShellDisposedCondition;
 import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
@@ -31,61 +32,89 @@ import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
 
 /**
  * @author Eugene Kuleshov
+ * @author Anton Kraev
  */
 public class PomEditorTest extends UITestCaseSWT {
 
-  private IUIContext ui;
+	private static final String POM_XML_TAB = "pom.xml";
+	private static final String OVERVIEW_TAB = "Overview";
+	private IUIContext ui;
 
-  protected void setUp() throws Exception {
-    super.setUp();
-    
-    ui = getUI();
-    
-    ui.close(new CTabItemLocator("Welcome"));
+	protected void setUp() throws Exception {
+		ui = getUI();
+	}
 
-    ui.click(new MenuItemLocator("Window/Open Perspective/Other..."));
-    ui.wait(new ShellShowingCondition("Open Perspective"));
-    ui.click(2, new TableItemLocator("Java( \\(default\\))?"));
-    ui.wait(new ShellDisposedCondition("Open Perspective"));
-  }
-  
-  public void testSamplePomEditor2() throws Exception {
-    ui.contextClick(new SWTWidgetLocator(Tree.class, new ViewLocator("org.eclipse.jdt.ui.PackageExplorer")),
-        "Ne&w/Maven Project");
-    ui.wait(new ShellShowingCondition("New Maven Project"));
-    ui.click(new ButtonLocator("Create a &simple project (skip archetype selection)"));
-    ui.click(new ButtonLocator("&Next >"));
-    ui.enterText("org.foo");
-    ui.keyClick(WT.TAB);
-    ui.enterText("test-pom");
-    ui.click(new ButtonLocator("&Finish"));
-    ui.wait(new ShellDisposedCondition("New Maven Project"));
+	protected void oneTimeSetup() throws Exception {
+		super.oneTimeSetup();
 
-    ui.click(2, new TreeItemLocator("test-pom/pom.xml", new ViewLocator("org.eclipse.jdt.ui.PackageExplorer")));
+		ui = getUI();
+		ui.close(new CTabItemLocator("Welcome"));
 
-    // ui.click(new MenuItemLocator("Window/Navigation/Maximize Active View or Editor"));
-    ui.keyClick(SWT.CTRL, 'm');
-    
-    ui.click(new CTabItemLocator("pom.xml"));
-    
-    // ui.click(new MenuItemLocator("Edit/Find\\\\/Replace\\.\\.\\.(.*)"));
-    ui.keyClick(SWT.CTRL, 'f');
-    ui.wait(new ShellShowingCondition("Find/Replace"));
+		ui.click(new MenuItemLocator("Window/Open Perspective/Other..."));
+		ui.wait(new ShellShowingCondition("Open Perspective"));
+		ui.click(2, new TableItemLocator("Java( \\(default\\))?"));
+		ui.wait(new ShellDisposedCondition("Open Perspective"));
 
-    ui.enterText("test-pom");
-    ui.keyClick(WT.TAB);
-    ui.enterText("test-pom1");
-    ui.keyClick(SWT.ALT, 'a');  // "replace all"
-    ui.close(new SWTWidgetLocator(Shell.class, "Find/Replace"));
-    ui.wait(new ShellDisposedCondition("Find/Replace"));
-    
-    ui.click(new CTabItemLocator("Overview"));
+		//create new project with POM
+		ui.contextClick(new SWTWidgetLocator(Tree.class, new ViewLocator(
+				"org.eclipse.jdt.ui.PackageExplorer")), "Ne&w/Maven Project");
+		ui.wait(new ShellShowingCondition("New Maven Project"));
+		ui.click(new ButtonLocator(
+				"Create a &simple project (skip archetype selection)"));
+		ui.click(new ButtonLocator("&Next >"));
+		ui.enterText("org.foo");
+		ui.keyClick(WT.TAB);
+		ui.enterText("test-pom");
+		ui.click(new ButtonLocator("&Finish"));
+		ui.wait(new ShellDisposedCondition("New Maven Project"));
+		ui.click(2, new TreeItemLocator("test-pom/pom.xml", new ViewLocator(
+				"org.eclipse.jdt.ui.PackageExplorer")));
+		ui.keyClick(SWT.CTRL, 'm');
+	}
 
-    NamedWidgetLocator artifactIdLocator = new NamedWidgetLocator("artifactId");
-    Text artifactIdText = (Text) ((WidgetReference) ui.find(artifactIdLocator)).getWidget();
-    assertNotNull("Can't find artifactIdText", artifactIdText);
-    ui.assertThat(new HasTextCondition(artifactIdLocator, "test-pom1"));
-  }
+	public void test1() throws Exception {
+		//test XML->FORM update of artifactId
+		ui.click(new CTabItemLocator(POM_XML_TAB));
+		replaceText("test-pom", "test-pom1");
+		ui.click(new CTabItemLocator(OVERVIEW_TAB));
+		testTextValue("artifactId", "test-pom1");
+	}
+
+	public void test2() throws Exception {
+		//test FORM->XML and XML->FORM update of parentArtifactId
+		ui.click(new SWTWidgetLocator(Label.class, "Parent"));
+		setTextValue("parentArtifactId", "parent2");
+		ui.click(new CTabItemLocator(POM_XML_TAB));
+		replaceText("parent2", "parent3");
+		ui.click(new CTabItemLocator(OVERVIEW_TAB));
+		testTextValue("parentArtifactId", "parent3");
+	}
+
+	private void testTextValue(String id, String value)
+			throws WidgetSearchException {
+		NamedWidgetLocator locator = new NamedWidgetLocator(id);
+		ui.assertThat(new HasTextCondition(locator, value));
+	}
+
+	private void setTextValue(String id, String value)
+			throws WidgetSearchException {
+		NamedWidgetLocator locator = new NamedWidgetLocator(id);
+		ui.setFocus(locator);
+		ui.keyClick(SWT.CTRL, 'a');
+		ui.enterText(value);
+	}
+
+	private void replaceText(String src, String target)
+			throws WaitTimedOutException, WidgetSearchException {
+		ui.keyClick(SWT.CTRL, 'f');
+		ui.wait(new ShellShowingCondition("Find/Replace"));
+
+		ui.enterText(src);
+		ui.keyClick(WT.TAB);
+		ui.enterText(target);
+		ui.keyClick(SWT.ALT, 'a'); // "replace all"
+		ui.close(new SWTWidgetLocator(Shell.class, "Find/Replace"));
+		ui.wait(new ShellDisposedCondition("Find/Replace"));
+	}
 
 }
-
