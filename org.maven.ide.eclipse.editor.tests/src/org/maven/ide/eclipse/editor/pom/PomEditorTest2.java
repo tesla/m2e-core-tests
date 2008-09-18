25e695bdd115591dd9d4bbc7bdd2520a7b3b449a
akraev <akraev@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-09-18 04:57:42 +0000

    MNGECLIPSE-746: add XML->model unit tests

--- org.maven.ide.eclipse.editor.tests/src/org/maven/ide/eclipse/editor/pom/PomEditorTest2.java
+++ org.maven.ide.eclipse.editor.tests/src/org/maven/ide/eclipse/editor/pom/PomEditorTest2.java
@@ -5,12 +5,11 @@ import org.eclipse.swt.SWT;
 import com.windowtester.runtime.WT;
 import com.windowtester.runtime.WaitTimedOutException;
 import com.windowtester.runtime.WidgetSearchException;
-import com.windowtester.runtime.locator.XYLocator;
 import com.windowtester.runtime.swt.condition.shell.ShellDisposedCondition;
 import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
 import com.windowtester.runtime.swt.locator.ButtonLocator;
-import com.windowtester.runtime.swt.locator.LabeledTextLocator;
 import com.windowtester.runtime.swt.locator.TableItemLocator;
+import com.windowtester.runtime.util.ScreenCapture;
 
 public class PomEditorTest2 extends PomEditorTestBase {
 
@@ -22,11 +21,8 @@ public class PomEditorTest2 extends PomEditorTestBase {
     expandSectionIfRequired("propertiesSection", "Properties");
     collapseSectionIfRequired("modulesSection", "Modules");
 
-    addProperty("p1", "p1");
-    addProperty("p2", "p2");
-    ui.click(new TableItemLocator("p1 : p1"));
-    ui.click(new ButtonLocator("Delete"));
-    ui.click(2, new TableItemLocator("p2 : p2"));
+    addProperty("pz", "pz");
+    ui.click(2, new TableItemLocator("pz : pz"));
     ui.wait(new ShellShowingCondition("Edit property"));
     ui.keyClick(SWT.CTRL, 'a');
     ui.enterText("p12");
@@ -35,24 +31,59 @@ public class PomEditorTest2 extends PomEditorTestBase {
     ui.enterText("p12");
     ui.click(new ButtonLocator("OK"));
     ui.wait(new ShellDisposedCondition("Edit property"));
+    addProperty("p2", "p2");
+    ui.click(new TableItemLocator("p12 : p12"));
+    ui.click(new ButtonLocator("Delete"));
     
     selectEditorTab(TAB_POM_XML);
     String editorText = getEditorText();
-    assertTrue(editorText, editorText.contains("<properties><p12>p12</p12></properties>"));
+    assertTrue(editorText, editorText.contains("<p2>p2</p2>"));
 
     ui.keyClick(SWT.CTRL, 'z');
     ui.keyClick(SWT.CTRL, 'z');
     ui.keyClick(SWT.CTRL, 'z');
     editorText = getEditorText();
-    assertTrue(editorText, editorText.contains("<properties><p2>p2</p2></properties>"));
-    ui.keyClick(SWT.CTRL, 'z');
+    assertTrue(editorText, editorText.contains("<pz>pz</pz>"));
     
-    //TODO: undo should do a complete clean
-//    ui.keyClick(SWT.CTRL, 'z');
-//    assertFalse(editorText, editorText.contains("<properties>"));
-//    ui.keyClick(SWT.CTRL, 'y');
-//    assertTrue(editorText, editorText.contains("<properties><p2>p2</p2></properties>"));
+    ui.keyClick(SWT.CTRL, 'z');
+    editorText = getEditorText();
+    assertFalse(editorText, editorText.contains("<properties>"));
+    ui.keyClick(SWT.CTRL, 'y');
+    editorText = getEditorText();
+    assertTrue(editorText, editorText.contains("<pz>pz</pz>"));
     ui.keyClick(SWT.CTRL, 's');
+  }
+
+  public void testPropertiesSectionXML2Model() throws Exception {
+    openPomFile(TEST_POM_POM_XML);
+
+    expandSectionIfRequired("propertiesSection", "Properties");
+    collapseSectionIfRequired("modulesSection", "Modules");
+
+    selectEditorTab(TAB_POM_XML);
+    
+    replaceText("<pz>pz</pz>", "<prop>hoho</prop>");
+    replaceText("<prop>hoho</prop>", "<prop>hoho</prop><prop1>hoho1</prop1>");
+
+    selectEditorTab(TAB_OVERVIEW);
+
+    ui.click(2, new TableItemLocator("prop : hoho"));
+    ui.wait(new ShellShowingCondition("Edit property"));
+    ui.click(new ButtonLocator("OK"));
+    ui.wait(new ShellDisposedCondition("Edit property"));
+    ui.click(2, new TableItemLocator("prop1 : hoho1"));
+    ui.wait(new ShellShowingCondition("Edit property"));
+    ui.click(new ButtonLocator("OK"));
+    ui.wait(new ShellDisposedCondition("Edit property"));
+
+    selectEditorTab(TAB_POM_XML);
+    replaceText("<prop>hoho</prop><prop1>hoho1</prop1>", "<prop>hoho</prop>");
+    selectEditorTab(TAB_OVERVIEW);
+
+    ui.click(2, new TableItemLocator("prop : hoho"));
+    ui.wait(new ShellShowingCondition("Edit property"));
+    ui.click(new ButtonLocator("OK"));
+    ui.wait(new ShellDisposedCondition("Edit property"));
   }
 
   private void addProperty(String name, String value) throws WidgetSearchException, WaitTimedOutException {
