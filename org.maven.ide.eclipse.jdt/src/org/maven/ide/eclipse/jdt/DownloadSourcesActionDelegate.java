0eef1d3135fa407cd6dcbeed905b361c1ab5a6ff
akraev <akraev@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-12-22 05:27:17 +0000

    automatic source/javadoc download from ClassFileEditor

---
+++ org.maven.ide.eclipse.jdt/src/org/maven/ide/eclipse/jdt/DownloadSourcesActionDelegate.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/package org.maven.ide.eclipse.jdt;
+
+import java.io.File;
+
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
+import org.eclipse.jdt.internal.ui.javaeditor.IClassFileEditorInput;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+
+import org.maven.ide.eclipse.MavenPlugin;
+import org.maven.ide.eclipse.core.MavenLogger;
+import org.maven.ide.eclipse.embedder.ArtifactKey;
+
+/**
+ * 
+ * DownloadSourcesActionDelegate
+ *
+ * @author Anton Kraev
+ */
+
+@SuppressWarnings("restriction")
+public class DownloadSourcesActionDelegate implements IEditorActionDelegate {
+
+  public void setActiveEditor(IAction action, IEditorPart part) {
+    if (part != null) {
+      try {
+        IClassFileEditorInput input = (IClassFileEditorInput) part.getEditorInput();
+        IJavaElement element = input.getClassFile();
+        while (element.getParent() != null) {
+          element = element.getParent();
+          if (element instanceof JarPackageFragmentRoot) {
+            JarPackageFragmentRoot root = (JarPackageFragmentRoot) element;
+            File f = root.getPath().toFile();
+            IJavaProject project = (IJavaProject) root.getParent();
+            ArtifactKey a = MavenPlugin.getDefault().getIndexManager().identify(f).getArtifactKey();
+            MavenPlugin.getDefault().getMavenProjectManager().downloadSources(project.getProject(), 
+                project.getPath(), a.getGroupId(), a.getArtifactId(), a.getVersion(), a.getClassifier(), true, true);
+            break;
+          }
+        }
+      } catch(Exception ex) {
+        MavenLogger.log("Cannot initiate source download", ex);
+      }
+    }
+  }
+
+  public void run(IAction action) {
+    // no need to do anything
+  }
+
+  public void selectionChanged(IAction action, ISelection selection) {
+    // no need to do anything
+  }
+
+}
