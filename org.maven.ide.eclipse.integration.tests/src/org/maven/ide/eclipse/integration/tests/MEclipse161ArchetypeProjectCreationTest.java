da2ea1b5158ec9b9292c9e5d7d4763d3cd1bf8f4
rseddon <rseddon@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-04-10 22:53:51 +0000

    fix maven-archetype-j2ee-simple test, also small patch to fix MNGECLIPSE-1300

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
@@ -10,7 +10,28 @@ package org.maven.ide.eclipse.integration.tests;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.ViewForm;
+import org.eclipse.swt.widgets.Composite;
+
+import com.windowtester.runtime.IUIContext;
+import com.windowtester.runtime.locator.IWidgetLocator;
+import com.windowtester.runtime.swt.condition.SWTIdleCondition;
+import com.windowtester.runtime.swt.condition.eclipse.JobsCompleteCondition;
+import com.windowtester.runtime.swt.condition.shell.ShellDisposedCondition;
+import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
+import com.windowtester.runtime.swt.locator.ButtonLocator;
+import com.windowtester.runtime.swt.locator.CTabItemLocator;
+import com.windowtester.runtime.swt.locator.FilteredTreeItemLocator;
+import com.windowtester.runtime.swt.locator.MenuItemLocator;
+import com.windowtester.runtime.swt.locator.NamedWidgetLocator;
+import com.windowtester.runtime.swt.locator.SWTWidgetLocator;
+import com.windowtester.runtime.swt.locator.TableCellLocator;
+import com.windowtester.runtime.swt.locator.TreeItemLocator;
+import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
 
 
 /**
@@ -21,7 +42,7 @@ public class MEclipse161ArchetypeProjectCreationTest extends UIIntegrationTestCa
   private IProject createArchetypeProjct(String archetypeName) throws Exception {
     return createArchetypeProjct(archetypeName, "project");
   }
- 
+
   public void testQuickStartCreate() throws Exception {
     IProject project = createArchetypeProjct("maven-archetype-quickstart");
     assertTrue(project.hasNature(JavaCore.NATURE_ID));
@@ -65,8 +86,66 @@ public class MEclipse161ArchetypeProjectCreationTest extends UIIntegrationTestCa
   public void testCreateSpringWS() throws Exception {
     createArchetypeProjct("spring-ws-archetype");
   }
-  
-  public void createJ2EESimple() throws Exception {
-    createArchetypeProjct("maven-archetype-j2ee-simple");
+
+  public void testCreateJ2EESimple() throws Exception {
+    String archetypeName = "maven-archetype-j2ee-simple";
+    String projectName = "project";
+    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+    assertFalse(project.exists());
+
+    IUIContext ui = getUI();
+    ui.click(new SWTWidgetLocator(ViewForm.class, new SWTWidgetLocator(CTabFolder.class, 0, new SWTWidgetLocator(
+        Composite.class))));
+    ui.click(new MenuItemLocator("File/New/Project..."));
+    ui.wait(new ShellShowingCondition("New Project"));
+    ui.click(new FilteredTreeItemLocator("Plug-in Project"));
+    ui.click(new FilteredTreeItemLocator("Maven/Maven Project"));
+    ui.click(new ButtonLocator("&Next >"));
+    ui.click(new ButtonLocator("&Next >"));
+    ui.click(new TableCellLocator(archetypeName, 2));
+    // NamedWidgetLocator table = new NamedWidgetLocator("archetypesTable");
+
+    ui.click(new ButtonLocator("&Next >"));
+    ui.wait(new SWTIdleCondition());
+    IWidgetLocator groupCombo = ui.find(new NamedWidgetLocator("groupId"));
+    ui.setFocus(groupCombo);
+    ui.enterText("org.sonatype.test");
+    ui.setFocus(ui.find(new NamedWidgetLocator("artifactId")));
+    ui.enterText(projectName);
+    ui.click(new ButtonLocator("&Finish"));
+    ui.wait(new ShellDisposedCondition("New Maven Project"));
+    Thread.sleep(5000); // Give builder a chance to start
+    ui.wait(new JobsCompleteCondition(), 240000);
+
+    project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+    assertTrue(project.exists());
+
+    // Bump compliance level to 1.5
+    openFile(project, "pom.xml");
+    getUI().click(new CTabItemLocator("pom.xml"));
+    getUI().wait(new JobsCompleteCondition(), 120000);
+    findText("</plugins");
+    getUI().keyClick(SWT.ARROW_LEFT);
+    getUI().enterText(
+        "<plugin><artifactId>maven-compiler-plugin</<version>2.0.2</<configuration><source>1.5</<target>1.5</</</");
+    getUI().keyClick(SWT.MOD1, 's');
+    Thread.sleep(5000);
+    getUI().wait(new JobsCompleteCondition(), 120000);
+    getUI().click(new TreeItemLocator("servlet*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
+    getUI().contextClick(new TreeItemLocator("servlet", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)),
+        "Maven/Update Project Configuration");
+    
+    // Update project configuration triggers multiple builds, and each build starts with a delay.
+    for (int i = 0; i < 10 && !new JobsCompleteCondition().test(); i++) {
+      getUI().wait(new JobsCompleteCondition(), 120000);
+      Thread.sleep(5000);
+    }
+
+    assertProjectsHaveNoErrors();
+    assertTrue("archtype project \"" + archetypeName + "\" created without Maven nature", project
+        .hasNature("org.maven.ide.eclipse.maven2Nature"));
+
+    ui.click(new TreeItemLocator(projectName + ".*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
+
   }
 }
