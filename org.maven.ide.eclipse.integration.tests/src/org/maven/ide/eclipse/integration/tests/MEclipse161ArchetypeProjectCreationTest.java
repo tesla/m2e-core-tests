425e44fbbf01c811ebda88b48dd01c50b9ba17f9
rseddon <rseddon@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-04-13 20:55:54 +0000

    restore tests mistakenly commented out.

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
@@ -43,49 +43,49 @@ public class MEclipse161ArchetypeProjectCreationTest extends UIIntegrationTestCa
     return createArchetypeProjct(archetypeName, "project");
   }
 
-//  public void testQuickStartCreate() throws Exception {
-//    IProject project = createArchetypeProjct("maven-archetype-quickstart");
-//    assertTrue(project.hasNature(JavaCore.NATURE_ID));
-//    IFile f = project.getFile("src/main/java/org/sonatype/test/project/App.java");
-//    assertTrue(f.exists());
-//    f = project.getFile("pom.xml");
-//    assertTrue(f.exists());
-//    f = project.getFile("src/test/java/org/sonatype/test/project/AppTest.java");
-//    assertTrue(f.exists());
-//
-//  }
-//
-//  public void testCreateMojo() throws Exception {
-//    createArchetypeProjct("maven-archetype-mojo");
-//  }
-//
-//  public void testCreatePortlet() throws Exception {
-//    createArchetypeProjct("maven-archetype-portlet");
-//  }
-//
-//  public void testCreateProfiles() throws Exception {
-//    createArchetypeProjct("maven-archetype-profiles");
-//  }
-//
-//  public void testCreateSite() throws Exception {
-//    createArchetypeProjct("maven-archetype-site");
-//  }
-//
-//  public void testCreateSiteSimple() throws Exception {
-//    createArchetypeProjct("maven-archetype-site-simple");
-//  }
-//
-//  public void testCreateSiteWebapp() throws Exception {
-//    createArchetypeProjct("maven-archetype-webapp");
-//  }
-//  
-//  public void testCreateStrutsStarter() throws Exception {
-//    createArchetypeProjct("struts2-archetype-starter");
-//  }
-//  
-//  public void testCreateSpringWS() throws Exception {
-//    createArchetypeProjct("spring-ws-archetype");
-//  }
+  public void testQuickStartCreate() throws Exception {
+    IProject project = createArchetypeProjct("maven-archetype-quickstart");
+    assertTrue(project.hasNature(JavaCore.NATURE_ID));
+    IFile f = project.getFile("src/main/java/org/sonatype/test/project/App.java");
+    assertTrue(f.exists());
+    f = project.getFile("pom.xml");
+    assertTrue(f.exists());
+    f = project.getFile("src/test/java/org/sonatype/test/project/AppTest.java");
+    assertTrue(f.exists());
+
+  }
+
+  public void testCreateMojo() throws Exception {
+    createArchetypeProjct("maven-archetype-mojo");
+  }
+
+  public void testCreatePortlet() throws Exception {
+    createArchetypeProjct("maven-archetype-portlet");
+  }
+
+  public void testCreateProfiles() throws Exception {
+    createArchetypeProjct("maven-archetype-profiles");
+  }
+
+  public void testCreateSite() throws Exception {
+    createArchetypeProjct("maven-archetype-site");
+  }
+
+  public void testCreateSiteSimple() throws Exception {
+    createArchetypeProjct("maven-archetype-site-simple");
+  }
+
+  public void testCreateSiteWebapp() throws Exception {
+    createArchetypeProjct("maven-archetype-webapp");
+  }
+  
+  public void testCreateStrutsStarter() throws Exception {
+    createArchetypeProjct("struts2-archetype-starter");
+  }
+  
+  public void testCreateSpringWS() throws Exception {
+    createArchetypeProjct("spring-ws-archetype");
+  }
 
   public void testCreateJ2EESimple() throws Exception {
     if (isEclipseVersion(3, 3)) {
