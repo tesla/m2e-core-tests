226b35db71e3fae5e530f5111971160b3d603f6f
rseddon <rseddon@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-07-15 18:30:53 +0000

    remove -fn

--- org.maven.ide.eclipse.integration.tests/run.cmd
+++ org.maven.ide.eclipse.integration.tests/run.cmd
@@ -7,4 +7,4 @@ set M2_HOME=c:\tycho
 set PATH=%M2_HOME%\bin;%PATH%
 
 cd ../org.maven.ide.eclipse.parent
-mvn clean install -fn -B  -Dtycho.targetPlatform=C:/platforms/eclipse-tp-e34/eclipse -Dmaven.test.failure.ignore=true -Dsurefire.useFile=false -Dm2e.system.test=true
+mvn clean install -B  -Dtycho.targetPlatform=C:/platforms/eclipse-tp-e34/eclipse -Dmaven.test.failure.ignore=true -Dsurefire.useFile=false -Dm2e.system.test=true
