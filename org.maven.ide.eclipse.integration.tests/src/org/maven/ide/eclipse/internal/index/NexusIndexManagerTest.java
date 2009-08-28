8c2767c1346092617665be090b8306b3bba41277
dyocum <dyocum@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-08-28 21:23:53 +0000

    temp changes

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/internal/index/NexusIndexManagerTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/internal/index/NexusIndexManagerTest.java
@@ -16,7 +16,12 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.maven.ide.components.pom.util.PomResourceImpl;
 import org.maven.ide.eclipse.MavenPlugin;
 import org.maven.ide.eclipse.integration.tests.UIIntegrationTestCase;
@@ -55,20 +60,39 @@ public class NexusIndexManagerTest extends UIIntegrationTestCase {
     super.tearDown();
   }
 
+  protected List<ArtifactRepository> remoteRepos = null;
   public void testPublicRepo() throws Exception{
-    
-    File emptySettingsFile = new File("src/org/maven/ide/eclipse/internal/index/empty_settings.xml");
+    final File emptySettingsFile = new File("src/org/maven/ide/eclipse/internal/index/empty_settings.xml");
     assertTrue(emptySettingsFile.exists());
 //    String publicMirrorPath = "";
 //    String publicNonMirroredPath = "";
     
+    WorkspaceJob job = new WorkspaceJob("!!!!!!!!!!! setting file"){
 
-    MavenPlugin.getDefault().getMaven().getMavenConfiguration().setUserSettingsFile(emptySettingsFile.getAbsolutePath());
-    assertEquals(MavenPlugin.getDefault().getMaven().getMavenConfiguration().getUserSettingsFile(), emptySettingsFile.getAbsolutePath());
+      public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+        try{
+          
+          MavenPlugin.getDefault().getMaven().getMavenConfiguration().setUserSettingsFile(emptySettingsFile.getAbsolutePath());
+          assertEquals(MavenPlugin.getDefault().getMaven().getMavenConfiguration().getUserSettingsFile(), emptySettingsFile.getAbsolutePath());
+          
+          remoteRepos = MavenPlugin.getDefault().getRemoteRepositories(monitor);
+        } catch(Exception e){
+          assertTrue(false);
+        }
+        return Status.OK_STATUS;
+      }
+    };
     
-    List<ArtifactRepository> remoteRepos = MavenPlugin.getDefault().getRemoteRepositories();
+    job.schedule();
+    Thread.sleep(3000);
+    for(ArtifactRepository repo : remoteRepos){
+      System.out.println("repos are: "+repo.getId()+ "at "+repo.getUrl());
+    }
     System.out.println("settings: "+MavenPlugin.getDefault().getMaven().getMavenConfiguration().getUserSettingsFile());
     List<ArtifactRepository> remoteRepositories = MavenPlugin.getDefault().getMaven().getEffectiveRepositories(remoteRepos);
+    for(ArtifactRepository rr : remoteRepositories){
+      System.out.println("repos are: "+rr.getId()+ "at "+rr.getUrl());
+    }
     assertEquals(1, remoteRepositories.size());
   }
   
