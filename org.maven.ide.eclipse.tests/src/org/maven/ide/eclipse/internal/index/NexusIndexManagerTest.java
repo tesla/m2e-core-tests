0c92455d03108be2b36361fd4cdb91953bd48d1a
dyocum <dyocum@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-09-09 20:59:54 +0000

    added tests for search functionality and index retrieval

--- org.maven.ide.eclipse.tests/src/org/maven/ide/eclipse/internal/index/NexusIndexManagerTest.java
+++ org.maven.ide.eclipse.tests/src/org/maven/ide/eclipse/internal/index/NexusIndexManagerTest.java
@@ -10,14 +10,17 @@ package org.maven.ide.eclipse.internal.index;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.maven.ide.eclipse.MavenPlugin;
 import org.maven.ide.eclipse.embedder.IMavenConfiguration;
-import org.maven.ide.eclipse.internal.index.IndexedArtifactGroup;
-import org.maven.ide.eclipse.internal.index.NexusIndexManager;
-import org.maven.ide.eclipse.internal.index.RepositoryInfo;
+import org.maven.ide.eclipse.index.IIndex;
+import org.maven.ide.eclipse.index.IndexedArtifact;
+import org.maven.ide.eclipse.index.IndexedArtifactFile;
 
 
 /**
@@ -31,7 +34,7 @@ public class NexusIndexManagerTest extends TestCase {
 
   private void waitForIndexJobToComplete() throws InterruptedException {
     indexManager.getIndexUpdateJob().join();
-  }
+  }  
 
   /**
    * Authentication was causing a failure for public (non-auth) repos. This test makes sure its ok.
@@ -61,6 +64,12 @@ public class NexusIndexManagerTest extends TestCase {
     assertNotNull(indexManager.getIndexingContext(repositories.get(0).getUrl()));
     assertNotNull(indexManager.getIndexingContext(repositories.get(1).getUrl()));
     assertNotNull(indexManager.getIndexingContext(repositories.get(2).getUrl()));
+    
+    NexusIndex workspaceIndex = indexManager.getIndex("workspace");
+    assertNotNull(workspaceIndex);
+    
+    NexusIndex localIndex = indexManager.getIndex("local");
+    assertNotNull(localIndex);
   }
 
   public void testPublicMirror() throws Exception {
@@ -76,7 +85,14 @@ public class NexusIndexManagerTest extends TestCase {
     assertEquals(publicRepoUrl, repositories.get(0).getUrl());
     assertNotNull(indexManager.getIndexingContext(publicRepoUrl));
     assertNull(indexManager.getIndexingContext(repositories.get(1).getUrl()));
+    
+    //make sure that the junit jar can be found in the public repo
+    NexusIndex index = indexManager.getIndex(publicRepoUrl);
+    assertNotNull(index);
+    Collection<IndexedArtifact> junitArtifact = index.find("junit", "junit", "3.8.1", "jar");
+    assertTrue(junitArtifact.size() > 0);
   }
+
 
   public void testPublicNonMirrored() throws Exception {
     final File nonMirroredRepoFile = new File(
@@ -88,11 +104,105 @@ public class NexusIndexManagerTest extends TestCase {
 
     ArrayList<RepositoryInfo> repositories = new ArrayList<RepositoryInfo>(indexManager.getRepositories());
     assertEquals(3, repositories.size());
-    assertEquals("http://repository.sonatype.org/content/repositories/eclipse", repositories.get(0).getUrl());
+    assertEquals("http://repository.sonatype.org/content/repositories/eclipse-snapshots/", repositories.get(0).getUrl());
     assertNotNull(indexManager.getIndexingContext(repositories.get(0).getUrl()));
-    assertEquals("http://repository.sonatype.org/content/repositories/eclipse-snapshots/", repositories.get(1).getUrl());
+    assertEquals("http://repository.sonatype.org/content/repositories/eclipse", repositories.get(1).getUrl());
     assertNotNull(indexManager.getIndexingContext(repositories.get(1).getUrl()));
     assertNull(indexManager.getIndexingContext(repositories.get(2).getUrl()));
   }
+  
+  public void testArtifactSearch() throws Exception {
+    final File settingsFile = new File("src/org/maven/ide/eclipse/internal/index/no_mirror_settings.xml");
+    assertTrue(settingsFile.exists());
 
+    mavenConfiguration.setUserSettingsFile(settingsFile.getCanonicalPath());
+    waitForIndexJobToComplete();
+    
+    Map<String, IndexedArtifact> search = indexManager.search("commons-logging", IIndex.SEARCH_ARTIFACT);
+    IndexedArtifact ia = search.get("null : null : commons-logging : commons-logging");
+    String gav = NexusIndexManager.getGAV(ia.getGroupId(), ia.getArtifactId(), "1.1.1", "jar");
+    assertNotNull(ia);
+    boolean hasVersion = false;
+    for(IndexedArtifactFile file : ia.getFiles()){
+      if("1.1.1".equals(file.version)){
+        hasVersion = true;
+        break;
+      }   
+    }
+    assertTrue(hasVersion);
+    
+    //TODO: do something with these
+    Map<String, IndexedArtifact> search2 = indexManager.search(gav, IIndex.SEARCH_ARTIFACT);  
+    search = indexManager.search("commons-logging-1.1.1", IIndex.SEARCH_ARTIFACT, IIndex.SEARCH_JAVADOCS);
+    search = indexManager.search("commons-logging-1.1.1", IIndex.SEARCH_ARTIFACT, IIndex.SEARCH_SOURCES);
+    search = indexManager.search("commons-logging-1.1.1", IIndex.SEARCH_ARTIFACT, IIndex.SEARCH_TESTS);
+    
+    Map<String, IndexedArtifact> noResultsSearch = indexManager.search("beepbeep-nothing", IIndex.SEARCH_ARTIFACT);
+    assertTrue(noResultsSearch.size() == 0);
+  }
+
+  
+  public void testClassSearch() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("TestCase", IIndex.SEARCH_CLASS_NAME);
+    assertTrue(search.size() > 0);
+
+    Map<String, IndexedArtifact> noResultsSearch = indexManager.search("BeepBeepNoClass", IIndex.SEARCH_CLASS_NAME);
+    assertTrue(noResultsSearch.size() == 0);
+  }
+
+  public void testPluginSearch() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("maven-war", IIndex.SEARCH_PLUGIN);
+    assertTrue(search.size() > 0);
+    
+    Map<String, IndexedArtifact> all = indexManager.search("*", IIndex.SEARCH_PLUGIN);
+    assertTrue(all.size() > 0);
+    
+    Map<String, IndexedArtifact> noResultsSearch = indexManager.search("TestCase", IIndex.SEARCH_PLUGIN);
+    assertTrue(noResultsSearch.size() == 0);
+  }
+  
+  public void testGroupSearch() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("org", IIndex.SEARCH_GROUP);
+    assertTrue(search.size() > 0);
+    
+    Map<String, IndexedArtifact> noResultsSearch = indexManager.search("org", IIndex.SEARCH_GROUP);
+    assertTrue(noResultsSearch.size() > 0);
+  }
+  
+  public void testArchetypeSearch() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("maven-archetype-quickstart", IIndex.SEARCH_ARCHETYPE);
+    assertTrue(search.size() == 1);
+    
+    //TODO: this should pass. add it back in when archetypes are working
+//    Map<String, IndexedArtifact> j2eeSearch = indexManager.search("maven-archetype-j2ee-simple", IIndex.SEARCH_ARCHETYPE);
+//    assertTrue(j2eeSearch.size() == 1);
+    
+    Map<String, IndexedArtifact> none = indexManager.search("maven-archetype-foobar", IIndex.SEARCH_ARCHETYPE);
+    assertTrue(none.size() == 0);
+  }
+  
+  public void testPackagingSearch() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("jar", IIndex.SEARCH_PACKAGING);
+    assertTrue(search.size() > 0);
+    
+    Map<String, IndexedArtifact> pomSearch = indexManager.search("pom", IIndex.SEARCH_PACKAGING);
+    assertTrue(pomSearch.size() > 0);
+    
+    Map<String, IndexedArtifact> noPackaging = indexManager.search("pomX", IIndex.SEARCH_PACKAGING);
+    assertTrue(noPackaging.size() == 0);
+  }
+ 
+  /**
+   * TODO: Stupid test at this point. Not sure what a valid sha1 search should look like.
+   * @throws Exception
+   */
+  public void testSha1Search() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("what-should-this-be", IIndex.SEARCH_SHA1);
+    assertTrue(search.size() == 0);
+  }
+  
+  public void testBogusSearchType() throws Exception {
+    Map<String, IndexedArtifact> search = indexManager.search("commons-logging", "BadSearchType");
+    assertTrue(search.size() == 0);
+  }
 }
