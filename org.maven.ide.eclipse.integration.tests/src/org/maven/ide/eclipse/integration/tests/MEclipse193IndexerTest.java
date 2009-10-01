b99017d7012fd47c74265999d52240ac26464c2c
dyocum <dyocum@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-10-01 23:47:18 +0000

    cleanup

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse193IndexerTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse193IndexerTest.java
@@ -166,31 +166,5 @@ public class MEclipse193IndexerTest extends UIIntegrationTestCase {
     ui.click(new TreeItemLocator("Local Repositories/Local repository.*/com",
         new ViewLocator("org.maven.ide.eclipse.views.MavenRepositoryView")));
   }
-//
-//  private void launchQuickFix(IProject project) throws Exception {
-//    //Workaround for Window tester bug, close & reopen tab to prevent editor from being in invalid state.
-//    getUI().close(new CTabItemLocator("App.java"));
-//    openFile(project, "src/main/java/org/sonatype/test/project/App.java");
-//
-//    //launch quick fix for SessionFactory dependency
-//    getUI().click(new TreeItemLocator("project.*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
-//    getUI().keyClick(SWT.MOD1 | SWT.SHIFT, 't');
-//    getUI().wait(new ShellShowingCondition("Open Type"));
-//    getUI().enterText("app");
-//    getUI().wait(new SWTIdleCondition());
-//    getUI().click(new ButtonLocator("OK"));
-//    getUI().wait(new ShellDisposedCondition("Open Type"));
-//    getUI().wait(new JobsCompleteCondition(), 60000);
-//
-//    getUI().keyClick(SWT.MOD1, '.'); // next annotation
-//
-//    getUI().keyClick(SWT.MOD1, '1');
-//    getUI().wait(new ShellShowingCondition(""));
-//    getUI().keyClick(SWT.END);
-//    getUI().keyClick(SWT.ARROW_UP);
-//
-//    getUI().keyClick(SWT.CR);
-//    getUI().wait(new ShellShowingCondition("Search in Maven repositories"));
-//    getUI().wait(new SWTIdleCondition());
-//  }
+
 }
