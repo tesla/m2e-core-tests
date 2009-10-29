26f98a0cdaa2d69de84eb28a1ae86b099198fa6b
dyocum <dyocum@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-10-29 21:55:48 +0000

    got rid of test based on outdated archetype

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse161ArchetypeProjectCreationTest.java
@@ -90,82 +90,83 @@ public class MEclipse161ArchetypeProjectCreationTest extends UIIntegrationTestCa
     createArchetypeProjct("spring-ws-archetype");
   }
 
-  public void testCreateJ2EESimple() throws Exception {
-    if (isEclipseVersion(3, 3)) {
-     // maven-archetype-j2ee-simple produces an empty ear deployment descriptor, which makes Eclipse 3.3 WTP blow up
-      return;
-    }
-    String archetypeName = "maven-archetype-j2ee-simple";
-    String projectName = "project";
-    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-    assertFalse(project.exists());
-
-    IUIContext ui = getUI();
-    ui.click(new SWTWidgetLocator(ViewForm.class, new SWTWidgetLocator(CTabFolder.class, 0, new SWTWidgetLocator(
-        Composite.class))));
-    ui.click(new MenuItemLocator("File/New/Project..."));
-    ui.wait(new ShellShowingCondition("New Project"));
-    ui.click(new FilteredTreeItemLocator("Plug-in Project"));
-    ui.click(new FilteredTreeItemLocator("Maven/Maven Project"));
-    //click the first next button
-    ui.click(new ButtonLocator("&Next >"));
-    //then the first page with only 'default' values
-    ui.click(new ButtonLocator("&Next >"));
-    
-    ui.wait(new SWTIdleCondition());
-    //now select the quickstart row
-    ui.click(new TableCellLocator(archetypeName, 2));
-    //and then click next
-    ui.click(new ButtonLocator("&Next >"));
-    IWidgetLocator groupCombo = ui.find(new NamedWidgetLocator("groupId"));
-    ui.setFocus(groupCombo);
-    ui.enterText("org.sonatype.test");
-    ui.setFocus(ui.find(new NamedWidgetLocator("artifactId")));
-    ui.enterText(projectName);
-    ui.click(new ButtonLocator("&Finish"));
-    ui.wait(new ShellDisposedCondition("New Maven Project"));
-
-    waitForAllBuildsToComplete();
-
-    project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-    assertTrue(project.exists());
-
-    // Bump compliance level to 1.5
-    openTextFile(project, "pom.xml");
-    waitForAllBuildsToComplete();
-    
-    //Expected to fail here as long as MNGECLIPSE-1795 is happening
-    findText("</plugins");
-    getUI().keyClick(SWT.ARROW_LEFT);
-    getUI().enterText(
-        "<plugin>" +
-          "<artifactId>maven-compiler-plugin</artifactId>" +
-          "<version>2.0.2</version>" +
-          "<configuration>" +
-            "<source>1.5</source>" +
-            "<target>1.5</target>" +
-          "</configuration>" +
-        "</plugin>");
-    getUI().keyClick(SWT.MOD1, 's');
-    Thread.sleep(5000);
-    getUI().wait(new JobsCompleteCondition(), 120000);
-    getUI().click(new TreeItemLocator("servlet*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
-    getUI().contextClick(new TreeItemLocator("servlet", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)),
-        "Maven/Update Project Configuration");
-    
-    waitForAllBuildsToComplete();
-    
-    getUI().click(new TreeItemLocator("ear*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
-    getUI().contextClick(new TreeItemLocator("ear", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)),
-        "Maven/Update Project Configuration");
-    
-    waitForAllBuildsToComplete();
-    
-    assertProjectsHaveNoErrors();
-    assertTrue("archtype project \"" + archetypeName + "\" created without Maven nature", project
-        .hasNature("org.maven.ide.eclipse.maven2Nature"));
-
-    ui.click(new TreeItemLocator(projectName + ".*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
-
-  }
+//  public void testCreateJ2EESimple() throws Exception {
+//    fail("https://issues.sonatype.org/browse/MNGECLIPSE-1795");
+//    if (isEclipseVersion(3, 3)) {
+//     // maven-archetype-j2ee-simple produces an empty ear deployment descriptor, which makes Eclipse 3.3 WTP blow up
+//      return;
+//    }
+//    String archetypeName = "maven-archetype-j2ee-simple";
+//    String projectName = "project";
+//    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+//    assertFalse(project.exists());
+//
+//    IUIContext ui = getUI();
+//    ui.click(new SWTWidgetLocator(ViewForm.class, new SWTWidgetLocator(CTabFolder.class, 0, new SWTWidgetLocator(
+//        Composite.class))));
+//    ui.click(new MenuItemLocator("File/New/Project..."));
+//    ui.wait(new ShellShowingCondition("New Project"));
+//    ui.click(new FilteredTreeItemLocator("Plug-in Project"));
+//    ui.click(new FilteredTreeItemLocator("Maven/Maven Project"));
+//    //click the first next button
+//    ui.click(new ButtonLocator("&Next >"));
+//    //then the first page with only 'default' values
+//    ui.click(new ButtonLocator("&Next >"));
+//    
+//    ui.wait(new SWTIdleCondition());
+//    //now select the quickstart row
+//    ui.click(new TableCellLocator(archetypeName, 2));
+//    //and then click next
+//    ui.click(new ButtonLocator("&Next >"));
+//    IWidgetLocator groupCombo = ui.find(new NamedWidgetLocator("groupId"));
+//    ui.setFocus(groupCombo);
+//    ui.enterText("org.sonatype.test");
+//    ui.setFocus(ui.find(new NamedWidgetLocator("artifactId")));
+//    ui.enterText(projectName);
+//    ui.click(new ButtonLocator("&Finish"));
+//    ui.wait(new ShellDisposedCondition("New Maven Project"));
+//
+//    waitForAllBuildsToComplete();
+//
+//    project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+//    assertTrue(project.exists());
+//
+//    // Bump compliance level to 1.5
+//    openTextFile(project, "pom.xml");
+//    waitForAllBuildsToComplete();
+//    
+//    //Expected to fail here as long as MNGECLIPSE-1795 is happening
+//    findText("</plugins");
+//    getUI().keyClick(SWT.ARROW_LEFT);
+//    getUI().enterText(
+//        "<plugin>" +
+//          "<artifactId>maven-compiler-plugin</artifactId>" +
+//          "<version>2.0.2</version>" +
+//          "<configuration>" +
+//            "<source>1.5</source>" +
+//            "<target>1.5</target>" +
+//          "</configuration>" +
+//        "</plugin>");
+//    getUI().keyClick(SWT.MOD1, 's');
+//    Thread.sleep(5000);
+//    getUI().wait(new JobsCompleteCondition(), 120000);
+//    getUI().click(new TreeItemLocator("servlet*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
+//    getUI().contextClick(new TreeItemLocator("servlet", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)),
+//        "Maven/Update Project Configuration");
+//    
+//    waitForAllBuildsToComplete();
+//    
+//    getUI().click(new TreeItemLocator("ear*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
+//    getUI().contextClick(new TreeItemLocator("ear", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)),
+//        "Maven/Update Project Configuration");
+//    
+//    waitForAllBuildsToComplete();
+//    
+//    assertProjectsHaveNoErrors();
+//    assertTrue("archtype project \"" + archetypeName + "\" created without Maven nature", project
+//        .hasNature("org.maven.ide.eclipse.maven2Nature"));
+//
+//    ui.click(new TreeItemLocator(projectName + ".*", new ViewLocator(PACKAGE_EXPLORER_VIEW_ID)));
+//
+//  }
 }
