4bd2d94b1209bb284ca2a89205484870e4b647cd
rseddon <rseddon@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-11-20 19:51:19 +0000

    Rewrite in attempt to improve reliability.

--- org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse178IssueTrackerTest.java
+++ org.maven.ide.eclipse.integration.tests/src/org/maven/ide/eclipse/integration/tests/MEclipse178IssueTrackerTest.java
@@ -1,11 +1,8 @@
 package org.maven.ide.eclipse.integration.tests;
 
-import java.io.File;
-
 import org.eclipse.swt.SWT;
 
 import com.windowtester.runtime.IUIContext;
-import com.windowtester.runtime.swt.condition.eclipse.JobsCompleteCondition;
 import com.windowtester.runtime.swt.condition.shell.ShellDisposedCondition;
 import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
 import com.windowtester.runtime.swt.locator.ButtonLocator;
@@ -15,57 +12,35 @@ import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
 
 public class MEclipse178IssueTrackerTest extends UIIntegrationTestCase {
 
-	private File tempDir;
 
-  /**
-	 * Main test method.
-	 *   <issueManagement>
-    <system>Jira</system>
-    <url>http://issues.sonatype.org</url>
-  </issueManagement>
-	 */
 	public void testIssueTracker() throws Exception {
 	  setXmlPrefs();
-    // Import the test project
-    tempDir = doImport("projects/ch07project.zip");
+	  createQuickstartProject("test-project");
 
 		IUIContext ui = getUI();
-		ui.click(new TreeItemLocator("simple-parent", new ViewLocator(
+		ui.click(new TreeItemLocator("test-project", new ViewLocator(
 				"org.eclipse.jdt.ui.PackageExplorer")));
-		ui.contextClick(new TreeItemLocator("simple-parent", new ViewLocator(
+		ui.contextClick(new TreeItemLocator("test-project", new ViewLocator(
 				"org.eclipse.jdt.ui.PackageExplorer")),
 				"Maven/Open Issue Tracker");
 		ui.wait(new ShellShowingCondition("Open Browser"));
 		ui.click(new ButtonLocator("OK"));
 		ui.wait(new ShellDisposedCondition("Open Browser"));
 		
-		openPomFile("simple-parent/pom.xml");
+		openPomFile("test-project/pom.xml");
 		ui.click(new CTabItemLocator("pom.xml"));
-		replaceText("</modules>", "</modules><issueManagement><system>JIRA</system><url>http://issues.sonatype.org</url></issueManagement>");
+		replaceText("</dependencies>", "</dependencies><issueManagement><system>JIRA</system><url>http://issues.sonatype.org</url></issueManagement>");
 		ui.keyClick(SWT.MOD1, 's');
-		try{
 		  waitForAllBuildsToComplete();
-		} catch(Throwable t){
-		  //wst seems to be barfing at times here. need to investigate
-		}
-		ui.click(new TreeItemLocator("simple-parent", new ViewLocator(
+		ui.click(new TreeItemLocator("test-project", new ViewLocator(
 				"org.eclipse.jdt.ui.PackageExplorer")));
-		ui.contextClick(new TreeItemLocator("simple-parent", new ViewLocator(
+		ui.contextClick(new TreeItemLocator("test-project", new ViewLocator(
 				"org.eclipse.jdt.ui.PackageExplorer")),
 				"Maven/Open Issue Tracker");
+		Thread.sleep(10000);
 		waitForAllBuildsToComplete();
 		ui.click(new CTabItemLocator("http://issues.sonatype.org"));
 	}
 
-  protected void tearDown() throws Exception {
-    
-    super.tearDown();
-    
-    if(tempDir != null && tempDir.exists()) {
-      deleteDirectory(tempDir);
-      tempDir = null;
-    }
-
-  }
 
 }
