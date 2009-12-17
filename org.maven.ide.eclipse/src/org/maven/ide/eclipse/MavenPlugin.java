fcb660661f110136eb7d79aa2b79e4b93ec2ac1c
dip <dip@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-12-17 15:49:05 +0000

    MNGECLIPSE-1952: rolled back

--- org.maven.ide.eclipse/src/org/maven/ide/eclipse/MavenPlugin.java
+++ org.maven.ide.eclipse/src/org/maven/ide/eclipse/MavenPlugin.java
@@ -38,7 +38,6 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
@@ -145,7 +144,7 @@ public class MavenPlugin extends AbstractUIPlugin implements IStartup {
    */
   private Map<Class<?>, Object> components = new HashMap<Class<?>, Object>();
 
-  protected MavenConsole console;
+  private MavenConsole console;
 
   private MavenModelManager modelManager;
 
@@ -212,11 +211,7 @@ public class MavenPlugin extends AbstractUIPlugin implements IStartup {
     MavenLogger.setLog(getLog());
     
     try {
-      Display.getDefault().syncExec(new Runnable() {
-        public void run() {
-          console = new MavenConsoleImpl(MavenImages.M2); //$NON-NLS-1$
-        }
-      });
+      this.console = new MavenConsoleImpl(MavenImages.M2); //$NON-NLS-1$
     } catch(RuntimeException ex) {
       MavenLogger.log(new Status(IStatus.ERROR, IMavenConstants.PLUGIN_ID, -1, "Unable to start console: " + ex.toString(), ex));
     }
