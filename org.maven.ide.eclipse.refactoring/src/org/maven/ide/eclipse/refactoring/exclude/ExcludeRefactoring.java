066691c5b7a241c4db2e1b06c3581f7cd11ecc1f
dip <dip@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2010-01-21 03:11:59 +0000

    MNGECLIPSE-1951: added a check to make sure parent dependencies are resolved

--- org.maven.ide.eclipse.refactoring/src/org/maven/ide/eclipse/refactoring/exclude/ExcludeRefactoring.java
+++ org.maven.ide.eclipse.refactoring/src/org/maven/ide/eclipse/refactoring/exclude/ExcludeRefactoring.java
@@ -19,9 +19,10 @@ import org.apache.maven.shared.dependency.tree.traversal.DependencyNodeVisitor;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.CompoundCommand;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
@@ -30,20 +31,23 @@ import org.maven.ide.components.pom.Exclusion;
 import org.maven.ide.components.pom.Model;
 import org.maven.ide.components.pom.impl.PomFactoryImpl;
 import org.maven.ide.eclipse.MavenPlugin;
+import org.maven.ide.eclipse.core.IMavenConstants;
 import org.maven.ide.eclipse.embedder.MavenModelManager;
 import org.maven.ide.eclipse.refactoring.AbstractPomRefactoring;
+import org.maven.ide.eclipse.refactoring.PomRefactoringException;
 import org.maven.ide.eclipse.refactoring.PomVisitor;
 import org.maven.ide.eclipse.refactoring.RefactoringModelResources;
+
 
 /**
  * Exclude artifact refactoring implementation
  * 
  * @author Anton Kraev
- *
  */
 public class ExcludeRefactoring extends AbstractPomRefactoring {
 
   private String excludedArtifactId;
+
   private String excludedGroupId;
 
   /**
@@ -59,14 +63,17 @@ public class ExcludeRefactoring extends AbstractPomRefactoring {
     return new PomVisitor() {
 
       @SuppressWarnings("unchecked")
-      public CompoundCommand applyChanges(RefactoringModelResources resources, IProgressMonitor pm) throws CoreException, IOException {
+      public CompoundCommand applyChanges(RefactoringModelResources resources, IProgressMonitor pm)
+          throws CoreException, IOException {
         final CompoundCommand command = new CompoundCommand();
 
         final List<Dependency> toRemove = new ArrayList<Dependency>();
-        
+
         Model model = resources.getTmpModel();
-        
+
         final List<Dependency> deps = model.getDependencies();
+
+        final IStatus[] status = new IStatus[] {null};
 
         pm.beginTask("Loading dependency tree", 1);
         MavenModelManager modelManager = MavenPlugin.getDefault().getMavenModelManager();
@@ -75,52 +82,64 @@ public class ExcludeRefactoring extends AbstractPomRefactoring {
         root.accept(new DependencyNodeVisitor() {
 
           private Dependency findDependency(String groupId, String artifactId) {
-            for (Dependency d: deps) {
-              if (d.getGroupId().equals(groupId) && d.getArtifactId().equals(artifactId)) {
+            for(Dependency d : deps) {
+              if(d.getGroupId().equals(groupId) && d.getArtifactId().equals(artifactId)) {
                 return d;
               }
             }
             return null;
           }
-          
+
           public boolean endVisit(DependencyNode arg0) {
-            return true;
+            return status[0] == null;
           }
 
           public boolean visit(DependencyNode node) {
             Artifact a = node.getArtifact();
-            if (a.getGroupId().equals(excludedGroupId) && a.getArtifactId().equals(excludedArtifactId)) {
+            if(a.getGroupId().equals(excludedGroupId) && a.getArtifactId().equals(excludedArtifactId)) {
               DependencyNode parent = node.getParent();
-              if (parent == null) {
+              if(parent == null) {
                 // do not touch itself
-                return false; 
+                return false;
               }
-              if (parent.getParent() == null) {
+              if(parent.getParent() == null) {
                 // need to remove first-level dependency
                 toRemove.add(findDependency(excludedGroupId, excludedArtifactId));
                 return false;
               }
-              while (parent.getParent().getParent() != null) {
+              while(parent.getParent().getParent() != null) {
                 parent = parent.getParent();
               }
-              if(node.getState() == DependencyNode.INCLUDED){
-                addExclusion(command, findDependency(parent.getArtifact().getGroupId(), parent.getArtifact().getArtifactId()));
+              if(node.getState() == DependencyNode.INCLUDED) {
+                String groupId = parent.getArtifact().getGroupId();
+                String artifactId = parent.getArtifact().getArtifactId();
+                Dependency parentDependency = findDependency(groupId, artifactId);
+                if(parentDependency == null) {
+                  status[0] = new Status(IStatus.ERROR, IMavenConstants.PLUGIN_ID, "Parent dependency not found for "
+                      + groupId + ':' + artifactId);
+                  return false;
+                }
+                addExclusion(command, parentDependency);
               } else {
                 return false;
               }
             }
             return true;
           }
-          
+
         });
-        
+
+        if(status[0] != null) {
+          throw new PomRefactoringException(status[0]);
+        }
+
         Iterator rem = toRemove.iterator();
-        while (rem.hasNext()) {
+        while(rem.hasNext()) {
           command.append(new RemoveCommand(editingDomain, model.getDependencies(), rem.next()));
         }
-        
+
         // XXX scan management as well
-        
+
         return command;
       }
 
@@ -144,7 +163,7 @@ public class ExcludeRefactoring extends AbstractPomRefactoring {
   public String getTitle() {
     return "Excluding " + excludedGroupId + ":" + excludedArtifactId + " from " + file.getParent().getName();
   }
-  
+
   public boolean scanAllArtifacts() {
     //do not scan other artifacts
     return false;
