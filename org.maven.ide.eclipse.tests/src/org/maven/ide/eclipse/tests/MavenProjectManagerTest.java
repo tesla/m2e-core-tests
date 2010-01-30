bc69eea291c4d43d7dbbc8291f18d834dc93bca9
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2010-01-30 20:19:22 +0000

    Tests fail on Linux due to inconsistent environment variable name in pom.xml and test java.

--- org.maven.ide.eclipse.tests/src/org/maven/ide/eclipse/tests/MavenProjectManagerTest.java
+++ org.maven.ide.eclipse.tests/src/org/maven/ide/eclipse/tests/MavenProjectManagerTest.java
@@ -881,7 +881,7 @@ public class MavenProjectManagerTest extends AsbtractMavenProjectTestCase {
   }
 
   public void testEnvironmentVariablesConsidered() throws Exception {
-    String tmpDir = System.getenv("TMP");
+    String tmpDir = System.getenv("TEMP");
     assertTrue("This test requires the environment variable TMP to be set", tmpDir != null);
 
     File systemJar = new File("projects/MNGECLIPSE-581/mngeclipse-581.jar");
