c090a1caccd4cb2e76a034f389a3eb4d14bf6405
bentmann <bentmann@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2010-02-18 13:38:30 +0000

    o Added helpers to configure Maven settings

---
+++ org.maven.ide.eclipse.tests.common/src/org/maven/ide/eclipse/tests/common/MavenHelpers.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.maven.ide.eclipse.tests.common;
+
+import java.io.File;
+
+import org.maven.ide.eclipse.MavenPlugin;
+import org.maven.ide.eclipse.embedder.IMavenConfiguration;
+
+
+public class MavenHelpers {
+
+  @SuppressWarnings("deprecation")
+  public static String setUserSettings(String settingsFile) {
+    if(settingsFile != null && settingsFile.length() > 0) {
+      settingsFile = new File(settingsFile).getAbsolutePath();
+    }
+    IMavenConfiguration mavenConfiguration = MavenPlugin.lookup(IMavenConfiguration.class);
+    String oldUserSettingsFile = mavenConfiguration.getUserSettingsFile();
+    mavenConfiguration.setUserSettingsFile(settingsFile);
+    return oldUserSettingsFile;
+  }
+
+  @SuppressWarnings("deprecation")
+  public static String getUserSettings() {
+    IMavenConfiguration mavenConfiguration = MavenPlugin.lookup(IMavenConfiguration.class);
+    return mavenConfiguration.getUserSettingsFile();
+  }
+
+}
