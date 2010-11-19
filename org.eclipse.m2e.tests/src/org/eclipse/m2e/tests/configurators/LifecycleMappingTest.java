34e2840b8b307ca04a21c40080dab4d37f365b20
vladt <vladt@sonatype.com>
2010-11-19 10:56:27 -0500

    MNGECLIPSE-2402: Unit tests

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/configurators/LifecycleMappingTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/configurators/LifecycleMappingTest.java
@@ -9,11 +9,14 @@
  *      Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
 
-
 package org.eclipse.m2e.tests.configurators;
 
 import java.util.List;
 
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.m2e.core.internal.project.CustomizableLifecycleMapping;
 import org.eclipse.m2e.core.internal.project.GenericLifecycleMapping;
 import org.eclipse.m2e.core.internal.project.MissingLifecycleMapping;
@@ -21,8 +24,11 @@ import org.eclipse.m2e.core.internal.project.MojoExecutionProjectConfigurator;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ILifecycleMapping;
+import org.eclipse.m2e.jdt.internal.JavaProjectConfigurator;
 import org.eclipse.m2e.tests.common.AbstractLifecycleMappingTest;
+import org.eclipse.m2e.tests.common.WorkspaceHelpers;
 
+@SuppressWarnings("restriction")
 public class LifecycleMappingTest extends AbstractLifecycleMappingTest {
   public void testGenericMapping() throws Exception {
     IMavenProjectFacade facade = importMavenProject("projects/lifecyclemapping", "generic/pom.xml");
@@ -30,6 +36,7 @@ public class LifecycleMappingTest extends AbstractLifecycleMappingTest {
     ILifecycleMapping lifecycleMapping = projectConfigurationManager.getLifecycleMapping(facade, monitor);
 
     assertTrue( lifecycleMapping instanceof GenericLifecycleMapping );
+    assertEquals(0, lifecycleMapping.getNotCoveredMojoExecutions(facade, monitor).size());
   }
 
   public void testCustomizableMapping() throws Exception {
@@ -42,6 +49,35 @@ public class LifecycleMappingTest extends AbstractLifecycleMappingTest {
     List<AbstractProjectConfigurator> configurators = lifecycleMapping.getProjectConfigurators(facade, monitor);
     assertEquals(2, configurators.size());
     assertTrue(configurators.get(1) instanceof MojoExecutionProjectConfigurator);
+
+    assertEquals(0, lifecycleMapping.getNotCoveredMojoExecutions(facade, monitor).size());
+  }
+
+  public void testCustomizableMappingNotComplete() throws Exception {
+    IMavenProjectFacade facade = importMavenProject("projects/lifecyclemapping", "customizableNotComplete/pom.xml");
+
+    ILifecycleMapping lifecycleMapping = projectConfigurationManager.getLifecycleMapping(facade, monitor);
+
+    assertTrue(lifecycleMapping instanceof CustomizableLifecycleMapping);
+
+    List<AbstractProjectConfigurator> configurators = lifecycleMapping.getProjectConfigurators(facade, monitor);
+    assertEquals(1, configurators.size());
+    assertTrue(configurators.get(0) instanceof JavaProjectConfigurator);
+
+    List<MojoExecution> notCoveredMojoExecutions = lifecycleMapping.getNotCoveredMojoExecutions(facade, monitor);
+    assertEquals(notCoveredMojoExecutions.toString(), 1, notCoveredMojoExecutions.size());
+    assertEquals("org.codehaus.modello:modello-maven-plugin:1.1:java {execution: standard}", notCoveredMojoExecutions
+        .get(0).toString());
+
+    IProject project = facade.getProject();
+    project.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+    waitForJobsToComplete();
+    List<IMarker> errorMarkers = WorkspaceHelpers.findErrorMarkers(project);
+    assertNotNull(errorMarkers);
+    assertEquals(WorkspaceHelpers.toString(errorMarkers), 1, errorMarkers.size());
+    assertEquals(
+        "Mojo execution not covered by lifecycle configuration: org.codehaus.modello:modello-maven-plugin:1.1:java {execution: standard}",
+        errorMarkers.get(0).getAttribute(IMarker.MESSAGE));
   }
 
   public void testMissingMapping() throws Exception {
@@ -51,5 +87,7 @@ public class LifecycleMappingTest extends AbstractLifecycleMappingTest {
 
     assertTrue(lifecycleMapping instanceof MissingLifecycleMapping);
     assertEquals("unknown-or-missing", ((MissingLifecycleMapping) lifecycleMapping).getMissingMappingId());
+
+    assertEquals(0, lifecycleMapping.getNotCoveredMojoExecutions(facade, monitor).size());
   }
 }
