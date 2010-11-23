efdcb15b218be0afee9508f9f084e6e596b1fc2b
Pascal Rapicault <pascal@sonatype.com>
2010-11-23 14:51:14 -0500

    Test that AHC is being used

---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/embedder/AHCWagonPresenceTest.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Sonatype, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.m2e.tests.embedder;
+
+import junit.framework.TestCase;
+
+import org.apache.maven.wagon.Wagon;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.internal.embedder.MavenImpl;
+
+
+public class AHCWagonPresenceTest extends TestCase {
+  public void testAHCisHere() throws ComponentLookupException, CoreException {
+    assertEquals("org.sonatype.maven.wagon.AhcWagon", ((MavenImpl) MavenPlugin.getDefault().getMaven())
+        .getPlexusContainer().lookup(Wagon.class, "http").getClass().getName());
+  }
+}
