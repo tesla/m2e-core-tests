720b602908447d712faaba01dc43b073c361f903
vladt <vladt@sonatype.com>
2010-12-03 17:11:13 -0500

    Rolled back previous commit

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/index/NexusIndexManagerSearchTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/index/NexusIndexManagerSearchTest.java
@@ -147,9 +147,15 @@ public class NexusIndexManagerSearchTest extends AbstractNexusIndexManagerTest {
   }
 
   public void testSearchGroups() throws CoreException {
-    Collection<IndexedArtifact> result = indexManager.getIndex((IProject) null).find(new SourcedSearchExpression(""),
-        null, null, null);
-    assertTrue(String.format("Wrong result set returned! (size=%s)", new Object[] {result.size()}), result.size() != 0);
+    try {
+      Collection<IndexedArtifact> result = indexManager.getIndex((IProject) null).find(new SourcedSearchExpression(""),
+          null, null, null);
+      assertTrue(String.format("Wrong result set returned! (size=%s)", new Object[] {result.size()}),
+          result.size() == 0);
+      fail("We should not get here!");
+    } catch(AssertionError e) {
+      // good
+    }
   }
 
   public void testSearchGroups2() throws CoreException {
