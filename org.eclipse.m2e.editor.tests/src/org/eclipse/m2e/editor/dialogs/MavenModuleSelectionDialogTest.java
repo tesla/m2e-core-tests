991e28729d0b51eb7b82ef71fa97eb0224bc3fae
dplatonoff <dmitry@platonoff.com>
2010-12-07 21:23:50 -0500

    MNGECLIPSE-729: simple maven module selection dialog test

---
+++ org.eclipse.m2e.editor.tests/src/org/eclipse/m2e/editor/dialogs/MavenModuleSelectionDialogTest.java
@@ -0,0 +1,89 @@
+
+package org.eclipse.m2e.editor.dialogs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.m2e.core.project.ResolverConfiguration;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.TreeItem;
+import org.junit.Test;
+
+
+public class MavenModuleSelectionDialogTest extends AbstractMavenProjectTestCase {
+
+  private static final String PROJECT1 = "project1";
+
+  private static final String PROJECT2 = "project2";
+
+  private static final String PROJECT3 = "project3";
+
+  private static final String MODULE1 = "module1";
+
+  @Test
+  public void testModuleSelection() throws Exception {
+    final IProject[] projects = importProjects("projects/modules", new String[] {//
+        PROJECT1 + "/pom.xml", //
+            PROJECT2 + "/pom.xml", //
+            PROJECT3 + "/pom.xml", //
+            PROJECT1 + "/" + MODULE1 + "/pom.xml"}, new ResolverConfiguration());
+
+    waitForJobsToComplete();
+
+    assertEquals("Imported projects", 4, projects.length);
+    assertEquals(PROJECT1, projects[0].getName());
+    assertEquals(PROJECT2, projects[1].getName());
+    assertEquals(PROJECT3, projects[2].getName());
+    assertEquals(MODULE1, projects[3].getName());
+
+    final Set<Object> excludedProjects = new HashSet<Object>();
+    excludedProjects.add(projects[0].getLocation());
+    excludedProjects.add(projects[3].getLocation());
+
+    Display.getDefault().syncExec(new Runnable() {
+      protected TreeViewer viewer;
+
+      protected Color disabledColor;
+
+      public void run() {
+        disabledColor = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
+
+        MavenModuleSelectionDialog dialog = new MavenModuleSelectionDialog(Display.getDefault().getActiveShell(),
+            excludedProjects) {
+          protected Control createDialogArea(Composite parent) {
+            Control control = super.createDialogArea(parent);
+            viewer = getViewer();
+            return control;
+          }
+        };
+        dialog.setBlockOnOpen(false);
+        dialog.open();
+        viewer.expandAll();
+
+        checkEnabledItems(viewer.getTree().getItems());
+      }
+
+      private void checkEnabledItems(TreeItem[] items) {
+        for(TreeItem item : items) {
+          Object data = item.getData();
+          assertTrue("Tree element data is IResource", data instanceof IResource);
+          
+          assertEquals("Tree element data is disabled if found in the known project list",
+              excludedProjects.contains(((IResource) data).getLocation()),
+              disabledColor.equals(((IColorProvider) viewer.getLabelProvider()).getForeground(data)));
+
+          checkEnabledItems(item.getItems());
+        }
+      }
+    });
+  }
+}
