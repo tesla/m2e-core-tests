68ebce295cd1dfe9e0d30e4120d110b29cb85e68
Matthew Piggott <mpiggott@sonatype.com>
2011-01-18 16:09:28 -0500

    Revert "MNGECLIPSE-2746 - Added tests for selecting catalog items by lifecycleId"
    This reverts commit f3ec78938d265ff6d2632dfe41f9c8f37e164383.

--- org.eclipse.m2e.discovery.tests/src/org/eclipse/m2e/discovery/tests/MavenDiscoveryTest.java
+++ org.eclipse.m2e.discovery.tests/src/org/eclipse/m2e/discovery/tests/MavenDiscoveryTest.java
@@ -12,8 +12,10 @@
 package org.eclipse.m2e.discovery.tests;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import junit.framework.TestCase;
 
@@ -23,7 +25,7 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.internal.p2.discovery.Catalog;
 import org.eclipse.equinox.internal.p2.discovery.DiscoveryCore;
 import org.eclipse.equinox.internal.p2.discovery.compatibility.BundleDiscoveryStrategy;
-import org.eclipse.equinox.internal.p2.discovery.model.CatalogItem;
+import org.eclipse.equinox.internal.p2.discovery.model.Tag;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.window.IShellProvider;
@@ -38,9 +40,9 @@ import org.junit.Test;
 @SuppressWarnings("restriction")
 public class MavenDiscoveryTest extends TestCase implements IShellProvider {
 
-  private static final String TYCHO_PACKAGE_TYPE = "eclipse-plugin";
+  static final Tag TAG_ONE = new Tag("one", "One");
 
-  private static final String TYCHO_ID = "com.sonatype.m2e.discovery-directories.lifecycles.tycho_feature";
+  static final Tag TAG_TWO = new Tag("two", "Two");
 
   private Catalog catalog;
 
@@ -56,16 +58,19 @@ public class MavenDiscoveryTest extends TestCase implements IShellProvider {
     catalog.getDiscoveryStrategies().add(new BundleDiscoveryStrategy());
 
     // Build the list of tags to show in the Wizard header
-    catalog.setTags(Collections.singletonList(MavenDiscovery.APPLICABLE_TAG));
+    List<Tag> tags = new ArrayList<Tag>(3);
+    tags.add(MavenDiscovery.APPLICABLE_TAG);
+    tags.add(TAG_ONE);
+    tags.add(TAG_TWO);
+    catalog.setTags(tags);
 
     // Create configuration for the catalog
     configuration = new MavenCatalogConfiguration();
     configuration.setShowTagFilter(true);
-    configuration.setSelectedTags(Collections.singletonList(MavenDiscovery.APPLICABLE_TAG));
+    configuration.setSelectedTags(tags);
     configuration.setShowInstalledFilter(false);
     configuration.setSelectedPackagingTypes(Collections.EMPTY_LIST);
     configuration.setSelectedMojos(Collections.EMPTY_LIST);
-    configuration.setSelectedLifecycleIds(Collections.EMPTY_LIST);
 
     shell = new Shell(Workbench.getInstance().getDisplay());
   }
@@ -77,113 +82,87 @@ public class MavenDiscoveryTest extends TestCase implements IShellProvider {
 
   @Test
   public void testCatalogUnselected() {
-    updateMavenCatalog();
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
 
-
-    assertFalse("CatalogItem should not be selected", getCatalogItem(TYCHO_ID).isSelected());
+    assertEquals("Unexpected number of catalog items", 1, catalog.getItems().size());
+    assertFalse("CatalogItem should not be selected", catalog.getItems().get(0).isSelected());
     assertFalse("CatalogItem should not be tagged Applicable",
-        getCatalogItem(TYCHO_ID).hasTag(MavenDiscovery.APPLICABLE_TAG));
+        catalog.getItems().get(0).hasTag(MavenDiscovery.APPLICABLE_TAG));
   }
 
   @Test
   public void testCatalogSelectedByPackaging() {
-    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {TYCHO_PACKAGE_TYPE}));
+    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {"eclipse-plugin"}));
 
-    updateMavenCatalog();
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
 
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
+    assertEquals("Unexpected number of catalog items", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be selected", catalog.getItems().get(0).isSelected());
   }
 
   @Test
   public void testCatalogTaggedApplicableByPackaging() {
-    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {TYCHO_PACKAGE_TYPE}));
+    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {"eclipse-plugin"}));
 
-    updateMavenCatalog();
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
 
-    assertTrue("CatalogItem should be tagged Applicable", getCatalogItem(TYCHO_ID)
-        .hasTag(MavenDiscovery.APPLICABLE_TAG));
+    assertEquals("Unexpected number of catalog items", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be tagged Applicable",
+        catalog.getItems().get(0).hasTag(MavenDiscovery.APPLICABLE_TAG));
   }
 
   @Test
   public void testCatalogSelectedByMojoExecution() {
     configuration.setSelectedMojos(Arrays.asList(new MojoExecution[] {getTychoMojoExecution()}));
 
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
+    assertEquals("Catalog entries missing", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be selected", catalog.getItems().get(0).isSelected());
   }
 
   @Test
   public void testCatalogTaggedApplicableByMojoExecution() {
     configuration.setSelectedMojos(Arrays.asList(new MojoExecution[] {getTychoMojoExecution()}));
 
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be tagged Applicable", getCatalogItem(TYCHO_ID)
-        .hasTag(MavenDiscovery.APPLICABLE_TAG));
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
+    assertEquals("Catalog entries missing", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be tagged Applicable",
+        catalog.getItems().get(0).hasTag(MavenDiscovery.APPLICABLE_TAG));
   }
 
   @Test
   public void testCatalogSelectedByBoth() {
     configuration.setSelectedMojos(Arrays.asList(new MojoExecution[] {getTychoMojoExecution()}));
-    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {TYCHO_PACKAGE_TYPE}));
+    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {"eclipse-plugin"}));
 
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
+    MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
+    mcv.createControl(shell);
+    mcv.updateCatalog();
+    assertEquals("Catalog entries missing", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be selected", catalog.getItems().get(0).isSelected());
   }
 
   @Test
   public void testCatalogTaggedApplicableByBoth() {
     configuration.setSelectedMojos(Arrays.asList(new MojoExecution[] {getTychoMojoExecution()}));
-    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {TYCHO_PACKAGE_TYPE}));
+    configuration.setSelectedPackagingTypes(Arrays.asList(new String[] {"eclipse-plugin"}));
 
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be tagged Applicable", getCatalogItem(TYCHO_ID)
-        .hasTag(MavenDiscovery.APPLICABLE_TAG));
-  }
-
-  @Test
-  public void testCatalogSelectedByLifecycleId() {
-    configuration.setSelectedLifecycleIds(Collections.singleton("unknown-or-missing"));
-
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
-  }
-
-  @Test
-  public void testCatalogSelectedByLifecycleIdWithPeriods() {
-    configuration.setSelectedLifecycleIds(Collections.singleton("my.lifecycle.id"));
-
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
-  }
-
-  @Test
-  public void testCatalogTaggedApplicableByLifecycleId() {
-    configuration.setSelectedLifecycleIds(Collections.singleton("unknown-or-missing"));
-
-    updateMavenCatalog();
-
-    assertTrue("CatalogItem should be selected", getCatalogItem(TYCHO_ID).isSelected());
-  }
-
-  private void updateMavenCatalog() {
     MavenCatalogViewer mcv = new MavenCatalogViewer(catalog, this, new RunnableContext(), configuration);
     mcv.createControl(shell);
     mcv.updateCatalog();
-  }
-
-  private CatalogItem getCatalogItem(String id) {
-    for (CatalogItem ci : catalog.getItems()) {
-      if (ci.getId().equals(id))
-        return ci;
-    }
-    fail("CatalogItem " + id + " not found in catalog");
-    return null;
+    assertEquals("Catalog entries missing", 1, catalog.getItems().size());
+    assertTrue("CatalogItem should be tagged Applicable",
+        catalog.getItems().get(0).hasTag(MavenDiscovery.APPLICABLE_TAG));
   }
 
   private static MojoExecution getTychoMojoExecution() {
