7bcb3453c7a83d09532e523c162d28c1f8c83052
Igor Fedorenko <igor@ifedorenko.com>
2011-02-04 21:11:03 -0500

    Updated java project configurator to use facade getMojoExecutions api
    Also added tests for getMojoExecution/getMojoExecutions after workspace
    restart, when cached execution plan is null.
    
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/project/MavenProjectFacadeTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/project/MavenProjectFacadeTest.java
@@ -1,6 +1,8 @@
 
 package org.eclipse.m2e.tests.project;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.List;
 
 import org.apache.maven.execution.MavenExecutionRequest;
@@ -39,6 +41,32 @@ public class MavenProjectFacadeTest extends AbstractMavenProjectTestCase {
 
   }
 
+  public void testGetMojoExecutionAfterWorkspaceRestart() throws Exception {
+    IProject project = importProject("projects/getmojoexecution/pom.xml");
+    assertNoErrors(project);
+
+    IMavenProjectFacade facade = plugin.getMavenProjectManager().create(project, monitor);
+    deserialize(facade);
+
+    MojoExecutionKey compileKey = null;
+    for(MojoExecutionKey executionKey : facade.getMojoExecutionMapping().keySet()) {
+      if("maven-compiler-plugin".equals(executionKey.getArtifactId()) && "compile".equals(executionKey.getGoal())) {
+        compileKey = executionKey;
+        break;
+      }
+    }
+
+    assertNotNull(compileKey);
+    MojoExecution compileMojo = facade.getMojoExecution(compileKey, monitor);
+
+    MavenExecutionRequest request = plugin.getMaven().createExecutionRequest(monitor);
+    MavenSession session = plugin.getMaven().createSession(request, facade.getMavenProject());
+
+    assertEquals("1.5", plugin.getMaven().getMojoParameterValue(session, compileMojo, "source", String.class));
+    assertEquals("1.6", plugin.getMaven().getMojoParameterValue(session, compileMojo, "target", String.class));
+
+  }
+
   public void testGetMojoExecutions() throws Exception {
     IProject project = importProject("projects/getmojoexecution/pom.xml");
     assertNoErrors(project);
@@ -46,7 +74,7 @@ public class MavenProjectFacadeTest extends AbstractMavenProjectTestCase {
     IMavenProjectFacade facade = plugin.getMavenProjectManager().create(project, monitor);
 
     List<MojoExecution> executions = facade.getMojoExecutions("org.apache.maven.plugins", "maven-compiler-plugin",
-        "compile", monitor);
+        monitor, "compile");
     assertEquals(executions.toString(), 1, executions.size());
 
     MavenExecutionRequest request = plugin.getMaven().createExecutionRequest(monitor);
@@ -54,5 +82,33 @@ public class MavenProjectFacadeTest extends AbstractMavenProjectTestCase {
 
     assertEquals("1.5", plugin.getMaven().getMojoParameterValue(session, executions.get(0), "source", String.class));
     assertEquals("1.6", plugin.getMaven().getMojoParameterValue(session, executions.get(0), "target", String.class));
+  }
+
+  public void testGetMojoExecutionsAfterWorkspaceRestart() throws Exception {
+    IProject project = importProject("projects/getmojoexecution/pom.xml");
+    assertNoErrors(project);
+
+    IMavenProjectFacade facade = plugin.getMavenProjectManager().create(project, monitor);
+    deserialize(facade);
+
+    List<MojoExecution> executions = facade.getMojoExecutions("org.apache.maven.plugins", "maven-compiler-plugin",
+        monitor, "compile");
+    assertEquals(executions.toString(), 1, executions.size());
+
+    MavenExecutionRequest request = plugin.getMaven().createExecutionRequest(monitor);
+    MavenSession session = plugin.getMaven().createSession(request, facade.getMavenProject());
+
+    assertEquals("1.5", plugin.getMaven().getMojoParameterValue(session, executions.get(0), "source", String.class));
+    assertEquals("1.6", plugin.getMaven().getMojoParameterValue(session, executions.get(0), "target", String.class));
+  }
+
+  private void deserialize(IMavenProjectFacade facade) throws IllegalArgumentException, IllegalAccessException {
+    // pretend it was deserialized from workspace state
+    for(Field field : facade.getClass().getDeclaredFields()) {
+      if(Modifier.isTransient(field.getModifiers())) {
+        field.setAccessible(true);
+        field.set(facade, null);
+      }
+    }
   }
 }
