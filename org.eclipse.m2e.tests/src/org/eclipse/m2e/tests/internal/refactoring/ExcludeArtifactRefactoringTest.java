b93680c6def219556cbb25e5ec42d5e6085e7739
Matthew Piggott <mpiggott@sonatype.com>
2011-03-08 17:19:57 -0500

    bug 337565 - Updated tests

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/refactoring/ExcludeArtifactRefactoringTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/refactoring/ExcludeArtifactRefactoringTest.java
@@ -3,8 +3,10 @@ package org.eclipse.m2e.tests.internal.refactoring;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
+import org.eclipse.ltk.core.refactoring.DocumentChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.RefactoringStatusEntry;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
@@ -26,6 +28,8 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.wst.sse.core.StructuredModelManager;
+import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 import org.junit.AfterClass;
 
 public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase {
@@ -72,7 +76,7 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 
 		status = refactoring.checkFinalConditions(monitor);
 		assertEquals("Expected FATAL status from checkFinalConditions: ", RefactoringStatus.FATAL, status.getSeverity());
-		assertMessage("Unexpected message", "No pom found for operation", status);
+		assertTrue("Refactoring Message", hasMessage("No pom found for operation", status));
 	}
 
 	/*
@@ -93,12 +97,12 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
 	}
 
 	/*
@@ -120,12 +124,12 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect module's pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect module's pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
 	}
 
 	/*
@@ -147,12 +151,12 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
-		assertAffected("Expected parent to be changed", getProject(projects, "workspaceParentWithDependencyModule").getFile("pom.xml"), change);
+		assertTrue("Expected parent to be changed", isAffected(getProject(projects, "workspaceParentWithDependencyModule").getFile("pom.xml"), change));
 
 		Change undo = change.perform(monitor);
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", getProject(projects, "workspaceParentWithDependencyModule"), ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(getProject(projects, "workspaceParentWithDependencyModule"), ROOT, VALID));
 	}
 
 	/*
@@ -173,12 +177,12 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect local pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect local pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
 	}
 
 	/*
@@ -195,17 +199,17 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 
 		status = refactoring.checkFinalConditions(monitor);
 		assertEquals("Expected FATAL status from checkFinalConditions: ", RefactoringStatus.ERROR, status.getSeverity());
-		assertMessage("Missing Dependency Expected", "Unable to locate source for dependency a-fake-artifact:that-should-never-exist:1.2.3 in the workspace.", status);
+		assertTrue("Missing Dependency Expected", hasMessage("Unable to locate source for dependency a-fake-artifact:that-should-never-exist:1.2.3 in the workspace.", status));
 
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
 	}
 
 	/*
@@ -227,13 +231,13 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect pom.xml", module.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect pom.xml", isAffected(module.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
-		assertExclusionSet("pom has exclusion set", editor, ROOT2, VALID3);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT2, VALID3));
 	}
 
 	/*
@@ -254,13 +258,13 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID2);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID2));
 	}
 
 	/*
@@ -281,20 +285,71 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 		Change change = refactoring.createChange(monitor);
 		assertTrue(change.isEnabled());
 
-		assertAffected("Expected change to affect test-pom.xml", project.getFile("pom.xml"), change);
+		assertTrue("Expected change to affect test-pom.xml", isAffected(project.getFile("pom.xml"), change));
 		Change undo = change.perform(monitor);
 		assertTrue("Editor is dirty", editor.isDirty());
 		assertNotNull("Undo Operation", undo);
 
-		assertExclusionSet("pom has exclusion set", editor, ROOT, VALID);
+		assertTrue("pom has exclusion set", hasExclusionSet(editor, ROOT, VALID));
+	}
+
+	public void testCopyDown() throws Exception {
+		IProject[] projects = importProjects(EXCLUDE_PATH + "/workspaceParentWithDependency", new String[] { "workspaceParentWithDependencyProject/pom.xml",
+				"workspaceParentWithDependencyModule/pom.xml" }, new ResolverConfiguration());
+		waitForJobsToComplete();
+		IProject module = getProject(projects, "workspaceParentWithDependencyModule");
+		new FindEditorRunnable(module.getFile("pom.xml")).open();
+		ExcludeArtifactRefactoring refactoring = createRefactoring(module.getFile("pom.xml"), new ArtifactKey[] { VALID });
+		RefactoringStatus status = refactoring.checkInitialConditions(monitor);
+		assertTrue("Expected OK status from checkInitialConditions: " + status.toString(), status.isOK());
+
+		status = refactoring.checkFinalConditions(monitor);
+		assertTrue("Expected OK status from checkFinalConditions: " + status.toString(), status.isOK());
+
+		Change change = refactoring.createChange(monitor);
+		assertTrue(change.isEnabled());
+		assertTrue("Expected module to be changed", isAffected(module.getFile("pom.xml"), change));
+
+		Change undo = change.perform(monitor);
+		assertNotNull("Undo Operation", undo);
+
+		assertTrue("project has exclusion set", hasExclusionSet(module, ROOT, VALID));
+	}
+
+	public void testPullUp() throws Exception {
+		IProject[] projects = importProjects(EXCLUDE_PATH + "/workspaceParent", new String[] { "workspaceParentProject/pom.xml", "workspaceParentModule/pom.xml" }, new ResolverConfiguration());
+		waitForJobsToComplete();
+		IProject module = getProject(projects, "workspaceParentModule");
+		IProject project = getProject(projects, "workspaceParentProject");
+		new FindEditorRunnable(module.getFile("pom.xml")).open();
+		ExcludeArtifactRefactoring refactoring = createRefactoring(module.getFile("pom.xml"), new ArtifactKey[] { VALID }, project.getFile("pom.xml"));
+		RefactoringStatus status = refactoring.checkFinalConditions(monitor);
+		assertTrue("Expected OK status from checkFinalConditions: " + status.toString(), status.isOK());
+
+		Change change = refactoring.createChange(monitor);
+		assertTrue(change.isEnabled());
+		assertTrue("Expected module to be changed", isAffected(module.getFile("pom.xml"), change));
+		assertTrue("Expected module to be changed", isAffected(project.getFile("pom.xml"), change));
+
+		Change undo = change.perform(monitor);
+		assertNotNull("Undo Operation", undo);
+
+		assertTrue("project has exclusion set", hasExclusionSet(project, ROOT, VALID));
+
 	}
 
 	private static ExcludeArtifactRefactoring createRefactoring(IFile pomFile, ArtifactKey[] keys) throws CoreException {
+		return createRefactoring(pomFile, keys, pomFile);
+	}
+
+	private static ExcludeArtifactRefactoring createRefactoring(IFile pomFile, ArtifactKey[] keys, IFile exclusionPoint) throws CoreException {
 		ExcludeArtifactRefactoring refactoring = new ExcludeArtifactRefactoring(pomFile, keys);
 		IMavenProjectFacade facade = MavenPlugin.getDefault().getMavenProjectManager().create(pomFile, true, monitor);
 		ParentGatherer g = new ParentGatherer(facade.getMavenProject(monitor), facade);
 		refactoring.setHierarchy(g.getParentHierarchy(monitor));
-		refactoring.setExclusionPoint(facade.getMavenProject(monitor));
+
+		IMavenProjectFacade excFacade = MavenPlugin.getDefault().getMavenProjectManager().create(exclusionPoint, true, monitor);
+		refactoring.setExclusionPoint(excFacade.getMavenProject(monitor));
 		return refactoring;
 	}
 
@@ -346,33 +401,44 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 	/*
 	 * Assert that a Change affects the given file
 	 */
-	protected static void assertAffected(String message, IFile file, Change change) {
+	protected static boolean isAffected(IFile file, Change change) {
 		if (change instanceof CompositeChange) {
+			IDocument open = getOpenDocument(file);
 			for (Change chg : ((CompositeChange) change).getChildren()) {
 				if (chg instanceof TextFileChange && file.equals(((TextFileChange) chg).getModifiedElement())) {
-					return;
+					return true;
+				} else if (chg instanceof DocumentChange && chg.getModifiedElement().equals(open)) {
+					return true;
 				}
 			}
 		}
-		fail(message);
+		return false;
+	}
+
+	private static IDocument getOpenDocument(IFile file) {
+		IStructuredModel model = StructuredModelManager.getModelManager().getExistingModelForEdit(file);
+		if (model != null) {
+			return model.getStructuredDocument();
+		}
+		return null;
 	}
 
 	/*
 	 * Assert the expected message on RefactoringStatus entry.
 	 */
-	protected static void assertMessage(String failMessage, String msg, RefactoringStatus status) {
+	protected static boolean hasMessage(String msg, RefactoringStatus status) {
 		for (RefactoringStatusEntry entry : status.getEntries()) {
 			if (entry.getMessage().equals(msg)) {
-				return;
+				return true;
 			}
 		}
-		fail(failMessage);
+		return false;
 	}
 
 	/*
-	 * Assert the editor has the given exclusion set
+	 * The editor has the given exclusion set
 	 */
-	protected static void assertExclusionSet(String msg, MavenPomEditor editor, ArtifactKey dependencyKey, ArtifactKey excluded) throws Exception {
+	protected static boolean hasExclusionSet(MavenPomEditor editor, ArtifactKey dependencyKey, ArtifactKey excluded) throws Exception {
 		Model model = editor.readProjectDocument();
 		Dependency d = null;
 		for (Dependency dep : model.getDependencies()) {
@@ -381,19 +447,21 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 				break;
 			}
 		}
-		assertNotNull("Model does not have dependency: " + dependencyKey, d);
+		if (d == null) {
+			return false;
+		}
 		for (Exclusion ex : d.getExclusions()) {
 			if (ex.getArtifactId().equals(excluded.getArtifactId()) && ex.getGroupId().equals(excluded.getGroupId())) {
-				return;
+				return true;
 			}
 		}
-		fail(msg);
+		return false;
 	}
 
 	/*
-	 * Assert the editor has the given exclusion set
+	 * The editor has the given exclusion set
 	 */
-	protected static void assertExclusionSet(String msg, IProject project, ArtifactKey dependencyKey, ArtifactKey excluded) throws Exception {
+	protected static boolean hasExclusionSet(IProject project, ArtifactKey dependencyKey, ArtifactKey excluded) throws Exception {
 		Model model = MavenPlugin.getDefault().getMavenModelManager().loadResource(project.getFile("pom.xml")).getModel();
 		Dependency d = null;
 		for (Dependency dep : model.getDependencies()) {
@@ -402,12 +470,14 @@ public class ExcludeArtifactRefactoringTest extends AbstractMavenProjectTestCase
 				break;
 			}
 		}
-		assertNotNull("Model does not have dependency: " + dependencyKey, d);
+		if (d == null) {
+			return false;
+		}
 		for (Exclusion ex : d.getExclusions()) {
 			if (ex.getArtifactId().equals(excluded.getArtifactId()) && ex.getGroupId().equals(excluded.getGroupId())) {
-				return;
+				return true;
 			}
 		}
-		fail(msg);
+		return false;
 	}
 }
