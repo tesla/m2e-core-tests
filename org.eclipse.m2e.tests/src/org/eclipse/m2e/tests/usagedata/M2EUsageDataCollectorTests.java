264a5563058cbb8ea92642f13a05a6c119cce137
Igor Fedorenko <igor@ifedorenko.com>
2011-04-08 00:07:39 -0400

    fixed intermittent NPE in M2EUsageDataCollectorTests
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/usagedata/M2EUsageDataCollectorTests.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/usagedata/M2EUsageDataCollectorTests.java
@@ -1,84 +1,78 @@
+
 package org.eclipse.m2e.tests.usagedata;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.epp.usagedata.internal.gathering.events.UsageDataEvent;
 import org.eclipse.epp.usagedata.internal.gathering.services.UsageDataService;
 
-import org.eclipse.m2e.internal.udc.Activator;
-import org.eclipse.m2e.internal.udc.PomImportMonitor;
+import org.eclipse.m2e.internal.udc.MavenUsageDataCollectorActivator;
 import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
 
 @SuppressWarnings("restriction")
 public class M2EUsageDataCollectorTests extends AbstractMavenProjectTestCase {
 
-	private TestUsageDataService usageDataService;
-	private PomImportMonitor monitor;
+  private TestUsageDataService usageDataService;
 
-	public void setUp() throws Exception {
-		super.setUp();
-		usageDataService = new TestUsageDataService();
+  public void setUp() throws Exception {
+    super.setUp();
+    usageDataService = new TestUsageDataService();
 
-		Thread.sleep(1000);
-		Field monitorField = Activator.class.getDeclaredField("monitor");
-		monitorField.setAccessible(true);
-		monitor = (PomImportMonitor) monitorField.get(Activator.getDefault());
-
-		monitor.startMonitoring(usageDataService);
-	}
-
-  public void tearDown() throws Exception {
-    try {
-      monitor.stopMonitoring();
-      monitor = null;
-      usageDataService = null;
-    } finally {
-      super.tearDown();
-    }
+    Thread.sleep(1000); // TODO this sucks, need a better way to plug test recorder
+    MavenUsageDataCollectorActivator.getDefault().setUsageDataService(usageDataService);
   }
 
-	public void testPackagingEvent() throws Exception {
-		importProject("projects/usagedatacollection/simple/pom.xml");
-		waitForJobsToComplete();
-		assertContains("Missing packaging type event", "pom", "m2e.packaging", "pom", "org.eclipse.m2e.core", "0.13.0");
-	}
+  public void tearDown() throws Exception {
+    usageDataService = null;
+    super.tearDown();
+  }
 
-	public void testPluginEvent() throws Exception {
-		importProject("projects/usagedatacollection/withplugins/pom.xml");
-		waitForJobsToComplete();
+  public void testPackagingEvent() throws Exception {
+    importProject("projects/usagedatacollection/simple/pom.xml");
+    waitForJobsToComplete();
+    assertContains("Missing packaging type event", "pom", "m2e.packaging", "pom", "org.eclipse.m2e.core", "0.13.0");
+  }
 
-		assertContains("Plugin Event", "clean:default-clean:clean", "m2e.plugins", "org.apache.maven.plugins:maven-clean-plugin:2.4.1", "org.eclipse.m2e.core", "0.13.0");
+  public void testPluginEvent() throws Exception {
+    importProject("projects/usagedatacollection/withplugins/pom.xml");
+    waitForJobsToComplete();
 
-	}
+    assertContains("Plugin Event", "clean:default-clean:clean", "m2e.plugins",
+        "org.apache.maven.plugins:maven-clean-plugin:2.4.1", "org.eclipse.m2e.core", "0.13.0");
 
-	private void assertContains(String message, String what, String kind, String description, String bundleId, String bundleVersion) {
-		StringBuilder e = new StringBuilder();
-		for (UsageDataEvent event : usageDataService.events) {
-			if (event.what.equals(what) && event.kind.equals(kind) && event.description.equals(description) && event.bundleId.equals(bundleId) && event.bundleVersion.equals(bundleVersion)) {
-				return;
-			}
-			e.append(toString(event)).append("\n");
-		}
-		fail(message + ": " + e.toString());
-	}
+  }
 
-	private static String toString(UsageDataEvent event) {
-		return new StringBuilder().append(event.what).append(',').append(event.kind).append(',').append(event.description).append(',').append(event.bundleId).append(',').append(event.bundleVersion)
-				.toString();
-	}
+  private void assertContains(String message, String what, String kind, String description, String bundleId,
+      String bundleVersion) {
+    StringBuilder e = new StringBuilder();
+    for(UsageDataEvent event : usageDataService.events) {
+      if(event.what.equals(what) && event.kind.equals(kind) && event.description.equals(description)
+          && event.bundleId.equals(bundleId) && event.bundleVersion.equals(bundleVersion)) {
+        return;
+      }
+      e.append(toString(event)).append("\n");
+    }
+    fail(message + ": " + e.toString());
+  }
 
-	private static class TestUsageDataService extends UsageDataService {
-		List<UsageDataEvent> events = new ArrayList<UsageDataEvent>();
+  private static String toString(UsageDataEvent event) {
+    return new StringBuilder().append(event.what).append(',').append(event.kind).append(',').append(event.description)
+        .append(',').append(event.bundleId).append(',').append(event.bundleVersion).toString();
+  }
 
-		public void recordEvent(String what, String kind, String description, String bundleId) {
-			recordEvent(what, kind, description, bundleId, null);
-		}
+  private static class TestUsageDataService extends UsageDataService {
+    List<UsageDataEvent> events = new ArrayList<UsageDataEvent>();
 
-		public void recordEvent(String what, String kind, String description, String bundleId, String bundleVersion) {
-			UsageDataEvent event = new UsageDataEvent(what, kind, description, bundleId, bundleVersion, System.currentTimeMillis());
-			events.add(event);
-		}
-	}
+    public void recordEvent(String what, String kind, String description, String bundleId) {
+      recordEvent(what, kind, description, bundleId, null);
+    }
+
+    public void recordEvent(String what, String kind, String description, String bundleId, String bundleVersion) {
+      UsageDataEvent event = new UsageDataEvent(what, kind, description, bundleId, bundleVersion,
+          System.currentTimeMillis());
+      events.add(event);
+    }
+  }
 }
