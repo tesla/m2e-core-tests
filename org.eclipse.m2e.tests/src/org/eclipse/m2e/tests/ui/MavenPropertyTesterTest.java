4ea0b81a1a80c4930c63c887da542500ef8f84e0
Fred Bricon <fbricon@gmail.com>
2012-07-18 23:28:51 +0200

    385422 : check build directory identification
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/ui/MavenPropertyTesterTest.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.m2e.tests.ui;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+
+import org.eclipse.m2e.core.project.ResolverConfiguration;
+import org.eclipse.m2e.core.ui.internal.actions.MavenPropertyTester;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+public class MavenPropertyTesterTest extends AbstractMavenProjectTestCase {
+
+  private static final String IS_BUILD_DIRECTORY = "isBuildDirectory"; //$NON-NLS-1$
+
+  public void testIsBuildDirectory() throws Exception {
+    IProject[] projects =  importProjects( "projects/385422_filterMavenBuildDir/",
+                                            new String[] {
+                                                "simple/pom.xml",
+                                                "nonDefaultBuildDir/pom.xml"
+                                                }, 
+                                            new ResolverConfiguration());
+    
+    IProject p1 = projects[0];
+    IProject p2 = projects[1];
+    IFolder targetDir;
+    IFolder buildDir;
+
+    MavenPropertyTester propertyTester = new MavenPropertyTester();
+    
+    //check default build directory :
+    targetDir = p1.getFolder("target");
+    buildDir = p1.getFolder("build");
+
+    assertTrue(targetDir +" should be identified as build dir", 
+                propertyTester.test(targetDir, IS_BUILD_DIRECTORY, null, null));
+    assertFalse(buildDir +" should not be identified as build dir", 
+                propertyTester.test(buildDir, IS_BUILD_DIRECTORY, null, null));
+    
+    //Check for custom build directory
+    targetDir = p2.getFolder("target");
+    buildDir = p2.getFolder("build");
+    
+    assertTrue(buildDir +" should be identified as build dir", 
+                propertyTester.test(buildDir, IS_BUILD_DIRECTORY, null, null));
+    assertFalse(targetDir +" should not be identified as build dir", 
+                propertyTester.test(targetDir, IS_BUILD_DIRECTORY, null, null));
+    
+  }
+  
+}
