c606103ee019cce3998aac8a5a6fe6e2c5a7ed54
youngm <youngm@gmail.com>
2012-09-02 14:36:15 -0600

    388508 Test to ensure that new setting to force import wizard to rename base directory works.

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/wizard/MavenImportWizardTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/wizard/MavenImportWizardTest.java
@@ -27,7 +27,7 @@ import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
 public class MavenImportWizardTest extends AbstractMavenProjectTestCase {
 
   /**
-   * When importing a project using MavenImportWizard we should never rename the directory we're importing from.
+   * When importing a project using MavenImportWizard we should never not the directory we're importing from by default.
    *
    * @throws Exception
    */
@@ -55,7 +55,7 @@ public class MavenImportWizardTest extends AbstractMavenProjectTestCase {
   }
 
   /**
-   * When importing a project located in the workspace root using MavenImportWizard the directory shouldn't be renamed.
+   * When importing a project located in the workspace root using MavenImportWizard the directory shouldn't be renamed by default.
    * Instead the project should be renamed to match the directory.
    *
    * @throws Exception
@@ -82,4 +82,35 @@ public class MavenImportWizardTest extends AbstractMavenProjectTestCase {
     assertEquals("not-artifact-id", project.getLocation().lastSegment());
     assertEquals("not-artifact-id", project.getName());
   }
+
+  /**
+   * When importing a maven project with rename required set to 'true'.  Ensure the folder is renamed to match the artifact-id.
+   *
+   * @throws Exception
+   */
+  public void testRenameOnMavenImport() throws Exception {
+    IWorkspaceRoot root = workspace.getRoot();
+
+    File src = new File("projects/projectimport/p001");
+    File dst = root.getLocation().append("not-artifact-id").toFile();
+    copyDir(src, dst);
+
+    MavenImportWizard wizard = new MavenImportWizard(new ProjectImportConfiguration(), Collections.singletonList(dst
+        .getAbsolutePath()));
+    wizard.setBasedirRemameRequired(true);
+    
+    Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+    WizardDialog dialog = new WizardDialog(shell, wizard);
+    dialog.setBlockOnOpen(false);
+    dialog.open();
+
+    wizard.performFinish();
+    dialog.close();
+    waitForJobsToComplete();
+
+    IProject project = workspace.getRoot().getProjects()[0];
+    assertEquals("p001", project.getLocation().lastSegment());
+    assertEquals("p001", project.getName());
+  }
+
 }
