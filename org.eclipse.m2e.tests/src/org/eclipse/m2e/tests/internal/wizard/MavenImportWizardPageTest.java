4a014f98f3ef05777c4b30edc9c9dbfcf993c90e
Igor Fedorenko <igor@ifedorenko.com>
2013-10-01 23:22:06 -0400

    408042 editable working set name, other fixes
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/wizard/MavenImportWizardPageTest.java
@@ -0,0 +1,81 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Igor Fedorenko
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Igor Fedorenko - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.m2e.tests.internal.wizard;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+import org.eclipse.m2e.core.project.ProjectImportConfiguration;
+import org.eclipse.m2e.core.ui.internal.wizards.MavenImportWizardPage;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+
+public class MavenImportWizardPageTest extends AbstractMavenProjectTestCase {
+
+  private WizardDialog dialog;
+
+  private Wizard wizard;
+
+  private MavenImportWizardPage page;
+
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+    wizard = new Wizard() {
+      public boolean performFinish() {
+        return false;
+      }
+    };
+    dialog = new WizardDialog(shell, wizard);
+
+    page = new MavenImportWizardPage(new ProjectImportConfiguration());
+
+    wizard.addPage(page);
+    dialog.setBlockOnOpen(false);
+  }
+
+  protected void tearDown() throws Exception {
+    dialog.close();
+    super.tearDown();
+  }
+
+  private void scanProjects(String pathname) throws IOException {
+    page.setLocations(Arrays.asList(new File(pathname).getCanonicalPath()));
+    dialog.open();
+    page.scanProjects();
+  }
+
+  public void test408042_simpleProject() throws Exception {
+    scanProjects("projects/408042_importWorkingSet/simple");
+    assertFalse(page.shouldCreateWorkingSet());
+    assertEquals("simple", page.getWorkingSetName());
+  }
+
+  public void test408042_multimoduleProject() throws Exception {
+    scanProjects("projects/408042_importWorkingSet/multimodule");
+    assertTrue(page.shouldCreateWorkingSet());
+    assertEquals("multimodule-parent", page.getWorkingSetName());
+  }
+
+  public void test408042_noRootProject() throws Exception {
+    scanProjects("projects/408042_importWorkingSet");
+    assertFalse(page.shouldCreateWorkingSet());
+    assertEquals("", page.getWorkingSetName());
+  }
+}
