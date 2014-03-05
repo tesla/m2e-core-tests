fa615f54cdc5fc4a9666acd6905234a4fa6423e5
Fred Bricon <fbricon@gmail.com>
2014-03-05 17:13:59 +0100

    427266 : default package should be empty
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/JavaUtilTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/JavaUtilTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2010 Sonatype, Inc.
+ * Copyright (c) 2008-2014 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ import org.eclipse.m2e.core.ui.internal.wizards.MavenProjectWizardArchetypeParam
 public class JavaUtilTest extends TestCase {
 
   public void testGetDefaultJavaPackage() {
-    assertEquals("foo", MavenProjectWizardArchetypeParametersPage.getDefaultJavaPackage("", ""));
+    assertEquals("", MavenProjectWizardArchetypeParametersPage.getDefaultJavaPackage("", ""));
     assertEquals("aaa.bbb", MavenProjectWizardArchetypeParametersPage.getDefaultJavaPackage("aaa", "bbb"));
     assertEquals("aaa.bbb.ccc.ddd",
         MavenProjectWizardArchetypeParametersPage.getDefaultJavaPackage("aaa.bbb.ccc", "ddd"));
