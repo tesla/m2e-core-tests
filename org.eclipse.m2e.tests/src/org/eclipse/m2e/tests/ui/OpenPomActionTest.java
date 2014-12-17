d1548c25b972c5f995b580185bd51e51a24032b4
Anton Tanasenko <atg.sleepless@gmail.com>
2014-12-17 23:01:29 +0200

    454759 Respect project repos in open pom action
    Signed-off-by: Anton Tanasenko <atg.sleepless@gmail.com>

---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/ui/OpenPomActionTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Takari, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Takari, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.m2e.tests.ui;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.ui.IEditorPart;
+
+import org.apache.maven.project.MavenProject;
+
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.ui.internal.actions.OpenPomAction;
+import org.eclipse.m2e.editor.pom.MavenPomEditor;
+import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
+
+
+public class OpenPomActionTest extends AbstractMavenProjectTestCase {
+
+  public void test454759_openEditor() throws Exception {
+    IProject project = importProject("projects/454759_openEditor/pom.xml");
+
+    waitForJobsToComplete();
+
+    IMavenProjectFacade facade = MavenPlugin.getMavenProjectRegistry().getProject(project);
+    MavenProject mavenProject = facade.getMavenProject(monitor);
+
+    OpenPomAction action = new OpenPomAction() {
+      protected void openDialog(String msg) {
+        // don't open any dialogs
+      }
+    };
+
+    IEditorPart editor = action.openPomEditor("test", "b", "1.0", null, monitor);
+    assertNull(editor);
+
+    editor = action.openPomEditor("test", "b", "1.0", mavenProject, monitor);
+    assertNotNull(editor);
+    assertTrue(editor instanceof MavenPomEditor);
+  }
+
+}
