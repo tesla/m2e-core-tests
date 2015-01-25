dbe57d0ad0bd270cc3fdbdc00c8f35727aeb6fb8
Fred Bricon <fbricon@gmail.com>
2015-01-25 10:25:10 -0500

    449495 : improve ProjectConfiguration sort order
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/lifecyclemapping/ConfigElementMock.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.m2e.tests.internal.lifecyclemapping;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IContributor;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.InvalidRegistryObjectException;
+
+
+//TODO Remove and use some Mocking framework instead
+class ConfigElementMock implements IConfigurationElement {
+
+  public Object createExecutableExtension(String propertyName) {
+    return null;
+  }
+
+  public String getAttribute(String name) throws InvalidRegistryObjectException {
+    return null;
+  }
+
+  public String getAttribute(String attrName, String locale) throws InvalidRegistryObjectException {
+    return null;
+  }
+
+  public String getAttributeAsIs(String name) {
+    return null;
+  }
+
+  public String[] getAttributeNames() {
+    return null;
+  }
+
+  public IConfigurationElement[] getChildren() {
+    return null;
+  }
+
+  public IConfigurationElement[] getChildren(String name) {
+    return null;
+  }
+
+  public IExtension getDeclaringExtension() {
+    return null;
+  }
+
+  public String getName() {
+    return null;
+  }
+
+  public Object getParent() {
+    return null;
+  }
+
+  public String getValue() {
+    return null;
+  }
+
+  public String getValue(String locale) {
+    return null;
+  }
+
+  public String getValueAsIs() {
+    return null;
+  }
+
+  public String getNamespace() {
+    return null;
+  }
+
+  public String getNamespaceIdentifier() {
+    return null;
+  }
+
+  public IContributor getContributor() {
+    return null;
+  }
+
+  public boolean isValid() {
+    return false;
+  }
+}
---
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/internal/lifecyclemapping/ProjectConfigurationElementSorterTest.java
@@ -0,0 +1,207 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *      Red Hat, Inc. - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.m2e.tests.internal.lifecyclemapping;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+
+import org.codehaus.plexus.util.dag.CycleDetectedException;
+
+import org.eclipse.m2e.core.internal.lifecyclemapping.ProjectConfigurationElementSorter;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+
+
+/**
+ * Sorts a list of {@link IPluginExecutionMetadata}s
+ */
+public class ProjectConfigurationElementSorterTest extends TestCase {
+
+  public void testSortConfigurators_HappyPath() throws Exception {
+
+    ConfiguratorMock c0 = ConfiguratorMock.create("c0", "c1, c2", "c4,");
+    ConfiguratorMock c1 = ConfiguratorMock.create("c1", "     c3,   ,   ");
+    ConfiguratorMock c2 = ConfiguratorMock.create("c2");
+    ConfiguratorMock c3 = ConfiguratorMock.createLegacy("c3", "c2");
+    ConfiguratorMock c4 = ConfiguratorMock.create("c4");
+    Map<String, IConfigurationElement> configElements = new HashMap<>(5);
+    for(ConfiguratorMock c : Arrays.asList(c0, c1, c2, c3, c4)) {
+      configElements.put(c.id, c);
+    }
+
+    ProjectConfigurationElementSorter sorter = new ProjectConfigurationElementSorter(configElements);
+
+    List<String> sortedConfigurators = sorter.getSortedConfigurators();
+
+    assertNotNull(sortedConfigurators);
+    assertEquals(configElements.size(), sortedConfigurators.size());
+    String msg = "invalid converter position in " + sortedConfigurators.toString();
+    assertEquals(msg, c2.id, sortedConfigurators.get(0));
+    assertEquals(msg, c3.id, sortedConfigurators.get(1));
+    assertEquals(msg, c1.id, sortedConfigurators.get(2));
+    assertEquals(msg, c0.id, sortedConfigurators.get(3));
+    assertEquals(msg, c4.id, sortedConfigurators.get(4));
+
+    assertEquals(0, sorter.getIncompleteConfigurators().size());
+  }
+
+  public void testSortConfigurators_CycleDetected() throws Exception {
+
+    ConfiguratorMock c0 = ConfiguratorMock.create("c0", "c1, c2", "c4");
+    ConfiguratorMock c1 = ConfiguratorMock.create("c1", "     c3,      ");
+    ConfiguratorMock c2 = ConfiguratorMock.create("c2", null, "c4");
+    ConfiguratorMock c3 = ConfiguratorMock.createLegacy("c3", "c2");
+    ConfiguratorMock c4 = ConfiguratorMock.create("c4", "", "c1");
+    Map<String, IConfigurationElement> configElements = new HashMap<>(5);
+    for(ConfiguratorMock c : Arrays.asList(c0, c1, c2, c3, c4)) {
+      configElements.put(c.id, c);
+    }
+
+    try {
+      new ProjectConfigurationElementSorter(configElements);
+      fail("Should have failed with CycleDetectedException");
+    } catch(CycleDetectedException e) {
+      //expected
+    }
+  }
+
+  public void testSortConfigurators_MissingFollowingConfigurator() throws Exception {
+
+    ConfiguratorMock c0 = ConfiguratorMock.create("c0");
+    ConfiguratorMock c1 = ConfiguratorMock.create("c1", "c0", "required*");
+    ConfiguratorMock c2 = ConfiguratorMock.create("c2", "c1", "optional");
+    Map<String, IConfigurationElement> configElements = new HashMap<>();
+    for(ConfiguratorMock c : Arrays.asList(c0, c1, c2)) {
+      configElements.put(c.id, c);
+    }
+
+    ProjectConfigurationElementSorter sorter = new ProjectConfigurationElementSorter(configElements);
+
+    List<String> sortedConfigurators = sorter.getSortedConfigurators();
+
+    assertNotNull(sortedConfigurators);
+    assertEquals(sortedConfigurators + " has an unexpected size", 1, sortedConfigurators.size());
+    String msg = "invalid converter position in " + sortedConfigurators;
+    assertEquals(msg, c0.id, sortedConfigurators.get(0));
+
+    Set<String> missingConfigurators = sorter.getMissingConfigurators();
+    assertEquals(1, missingConfigurators.size());
+    assertEquals("required", missingConfigurators.iterator().next());
+
+    Map<String, String> incompleteMetadatasMap = sorter.getIncompleteConfigurators();
+    Set<String> incompleteMetadatas = incompleteMetadatasMap.keySet();
+    String result = incompleteMetadatas.toString();
+    assertEquals(result, 2, incompleteMetadatas.size());
+    assertTrue(c1.id + " is missing from " + result, incompleteMetadatas.contains(c1.id));
+    assertTrue(c2.id + " is missing from " + result, incompleteMetadatas.contains(c2.id));
+  }
+
+  public void testSortConfigurators_MissingPreviousConfigurator() throws Exception {
+
+    ConfiguratorMock c0 = ConfiguratorMock.create("c0", "optional?");
+    ConfiguratorMock c1 = ConfiguratorMock.create("c1", "c0", "c2");
+    ConfiguratorMock c2 = ConfiguratorMock.create("c2", "required");
+    ConfiguratorMock c3 = ConfiguratorMock.create("c3", "c2", "c5");
+    ConfiguratorMock c4 = ConfiguratorMock.create("c4", "c1");
+    ConfiguratorMock c5 = ConfiguratorMock.create("c5", "c4");
+    ConfiguratorMock c6 = ConfiguratorMock.create("c6", "c3");
+    Map<String, IConfigurationElement> configElements = new HashMap<>();
+    for(ConfiguratorMock c : Arrays.asList(c0, c1, c2, c3, c4, c5, c6)) {
+      configElements.put(c.id, c);
+    }
+
+    ProjectConfigurationElementSorter sorter = new ProjectConfigurationElementSorter(configElements);
+    List<String> sortedConfigurators = sorter.getSortedConfigurators();
+
+    assertNotNull(sortedConfigurators);
+    assertEquals(sorter.getIncompleteConfigurators() + " has an unexpected size", 3, sorter
+        .getIncompleteConfigurators().size());//c2 (missing required), c3 (depends on c2), c6 (depends on c3)
+    String msg = "invalid converter position in " + sortedConfigurators.toString();
+    assertEquals(msg, c0.id, sortedConfigurators.get(0));
+    assertEquals(msg, c1.id, sortedConfigurators.get(1));
+    assertEquals(msg, c4.id, sortedConfigurators.get(2));
+    assertEquals(msg, c5.id, sortedConfigurators.get(3));
+
+    Set<String> missingConfigurators = sorter.getMissingConfigurators();
+    assertEquals(1, missingConfigurators.size());
+    assertEquals("required", missingConfigurators.iterator().next());
+
+    Map<String, String> incompleteMetadatasMap = sorter.getIncompleteConfigurators();
+    Set<String> incompleteMetadatas = incompleteMetadatasMap.keySet();
+    String result = incompleteMetadatas.toString();
+    assertEquals(result, 3, incompleteMetadatas.size());
+    assertTrue(c2.id + " is missing from " + result, incompleteMetadatas.contains(c2.id));
+    assertTrue(c3.id + " is missing from " + result, incompleteMetadatas.contains(c3.id));
+    assertTrue(c6.id + " is missing from " + result, incompleteMetadatas.contains(c6.id));
+  }
+
+  private static class ConfiguratorMock extends ConfigElementMock {
+
+    String id;
+
+    private String after;
+
+    private String before;
+
+    private String secondaryTo;
+
+    public ConfiguratorMock(String id) {
+      this.id = id;
+    }
+
+    static ConfiguratorMock create(String id) {
+      return new ConfiguratorMock(id);
+    }
+
+    static ConfiguratorMock createLegacy(String id, String secondaryTo) {
+      ConfiguratorMock mock = create(id);
+      mock.secondaryTo = secondaryTo;
+      return mock;
+    }
+
+    static ConfiguratorMock create(String id, String runsAfter) {
+      ConfiguratorMock mock = create(id);
+      mock.after = runsAfter;
+      return mock;
+    }
+
+    static ConfiguratorMock create(String id, String runsAfter, String runsBefore) {
+      ConfiguratorMock mock = create(id, runsAfter);
+      mock.before = runsBefore;
+      return mock;
+    }
+
+    public String getAttribute(String attrName) {
+      switch(attrName) {
+        case "runsAfter":
+          return after;
+        case "runsBefore":
+          return before;
+        case "secondaryTo":
+          return secondaryTo;
+        default:
+          return null;
+      }
+    }
+
+    public String toString() {
+      return id;
+    }
+  }
+
+}
