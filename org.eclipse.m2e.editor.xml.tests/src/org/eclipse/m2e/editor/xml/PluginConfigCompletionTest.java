7e144a47c46b3274ff404c9a8db8c617baeaccf7
Anton Tanasenko <atg.sleepless@gmail.com>
2015-02-07 16:47:17 +0200

    459362 Configuration content assist support for subclassed list items
    Signed-off-by: Anton Tanasenko <atg.sleepless@gmail.com>

--- org.eclipse.m2e.editor.xml.tests/src/org/eclipse/m2e/editor/xml/PluginConfigCompletionTest.java
+++ org.eclipse.m2e.editor.xml.tests/src/org/eclipse/m2e/editor/xml/PluginConfigCompletionTest.java
@@ -138,7 +138,7 @@ public class PluginConfigCompletionTest extends AbstractCompletionTest {
 
       PomContentAssistProcessor processor = new PomContentAssistProcessor(sourceViewer);
       ICompletionProposal[] proposals = processor.computeCompletionProposals(sourceViewer, offset);
-      assertEquals("Proposal count", 18+2, proposals.length);
+      assertEquals("Proposal count", 14+2, proposals.length);
   }
   
   public void testImplementationListItemNameCompletion() throws Exception {
@@ -152,5 +152,53 @@ public class PluginConfigCompletionTest extends AbstractCompletionTest {
       assertEquals("Proposal count", 1 + 2, proposals.length);
       
       assertEquals("List item proposal", "availableVersion", proposals[0].getDisplayString());
+  }
+  
+  public void testSubclassListItems() throws Exception {
+      initViewer();
+      
+      String docString = sourceViewer.getDocument().get();
+      int offset = docString.indexOf("</modules>");
+
+      PomContentAssistProcessor processor = new PomContentAssistProcessor(sourceViewer);
+      ICompletionProposal[] proposals = processor.computeCompletionProposals(sourceViewer, offset);
+      assertEquals("Proposal count", 12 + 2, proposals.length);
+      
+      assertEquals("Subclass item", "appClientModule", proposals[0].getDisplayString());
+      assertEquals("Subclass item", "ejb3Module", proposals[1].getDisplayString());
+      assertEquals("Subclass item", "ejbClientModule", proposals[2].getDisplayString());
+      assertEquals("Subclass item", "ejbModule", proposals[3].getDisplayString());
+      assertEquals("Subclass item", "harModule", proposals[4].getDisplayString());
+      assertEquals("Subclass item", "jarModule", proposals[5].getDisplayString());
+      assertEquals("Subclass item", "javaModule", proposals[6].getDisplayString());
+      assertEquals("Subclass item", "parModule", proposals[7].getDisplayString());
+      assertEquals("Subclass item", "rarModule", proposals[8].getDisplayString());
+      assertEquals("Subclass item", "sarModule", proposals[9].getDisplayString());
+      assertEquals("Subclass item", "webModule", proposals[10].getDisplayString());
+      assertEquals("Subclass item", "wsrModule", proposals[11].getDisplayString());
+  }
+  
+  public void testM2ELifecycleMappingConfiguration() throws Exception {
+      initViewer();
+      
+      PomContentAssistProcessor processor = new PomContentAssistProcessor(sourceViewer);
+      
+      String docString = sourceViewer.getDocument().get();
+      
+      int offset = docString.indexOf("</lifecycleMappingMetadata>");
+      ICompletionProposal[] proposals = processor.computeCompletionProposals(sourceViewer, offset);
+      assertEquals("Proposal count", 1 + 2, proposals.length);
+      assertEquals("List item proposal", "pluginExecutions", proposals[0].getDisplayString());
+      
+      offset = docString.indexOf("</pluginExecutionFilter>");
+      proposals = processor.computeCompletionProposals(sourceViewer, offset);
+      assertEquals("Proposal count", 4 + 2, proposals.length);
+
+      offset = docString.indexOf("</execute>");
+      proposals = processor.computeCompletionProposals(sourceViewer, offset);
+      assertEquals("Proposal count", 2 + 2, proposals.length);
+      assertEquals("Execute proposal", "runOnConfiguration", proposals[0].getDisplayString());
+      assertEquals("Execute proposal", "runOnIncremental", proposals[1].getDisplayString());
+
   }
 }
