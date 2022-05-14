d74ab38782bf89dc31ca7390aaddb2f5b6010bea
Hannes Wellmann <wellmann.hannes1@gmx.net>
2022-05-14 13:30:32 +0200

    Use IProfileManager service in tests

--- org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/profiles/AbstractMavenProfileTest.java
+++ org.eclipse.m2e.tests/src/org/eclipse/m2e/tests/profiles/AbstractMavenProfileTest.java
@@ -17,6 +17,9 @@ import java.io.File;
 
 import org.junit.After;
 import org.junit.Before;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
 
 import org.eclipse.core.resources.IProject;
 
@@ -24,7 +27,6 @@ import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.internal.IMavenConstants;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.profiles.core.internal.IProfileManager;
-import org.eclipse.m2e.profiles.core.internal.MavenProfilesCoreActivator;
 import org.eclipse.m2e.tests.common.AbstractMavenProjectTestCase;
 
 
@@ -34,13 +36,18 @@ public abstract class AbstractMavenProfileTest extends AbstractMavenProjectTestC
 
   private String originalSettings;
 
+  private ServiceTracker<IProfileManager, IProfileManager> profileManagerTracker;
+
   @Override
   @Before
   public void setUp() throws Exception {
     super.setUp();
     originalSettings = mavenConfiguration.getUserSettingsFile();
     mavenConfiguration.setUserSettingsFile(new File("settings_profiles.xml").getCanonicalPath());
-    profileManager = MavenProfilesCoreActivator.getDefault().getProfileManager();
+    BundleContext context = FrameworkUtil.getBundle(AbstractMavenProfileTest.class).getBundleContext();
+    profileManagerTracker = new ServiceTracker<>(context, IProfileManager.class, null);
+    profileManagerTracker.open();
+    profileManager = profileManagerTracker.getService();
   }
 
   protected IMavenProjectFacade getFacade(IProject project) {
@@ -54,6 +61,7 @@ public abstract class AbstractMavenProfileTest extends AbstractMavenProjectTestC
     if(originalSettings != null) {
       mavenConfiguration.setUserSettingsFile(originalSettings);
     }
+    profileManagerTracker.close();
     super.tearDown();
   }
 
